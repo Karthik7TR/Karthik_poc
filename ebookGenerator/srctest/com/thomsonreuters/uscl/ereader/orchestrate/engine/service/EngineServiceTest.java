771c30949e1b61a799ba9ac571866f4d9d0965b4
c114868 <c114868@tlr.tfs.local>
2011-12-12 17:59:58 +0000

    Updated to verify state of mock objects within each test.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C177379

--- ebookGenerator/srctest/com/thomsonreuters/uscl/ereader/orchestrate/engine/service/EngineServiceTest.java
+++ ebookGenerator/srctest/com/thomsonreuters/uscl/ereader/orchestrate/engine/service/EngineServiceTest.java
@@ -21,7 +21,6 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.JobOperator;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.thomsonreuters.uscl.ereader.orchestrate.core.JobRunRequest;
 import com.thomsonreuters.uscl.ereader.orchestrate.core.engine.EngineConstants;
@@ -36,47 +35,43 @@ public class EngineServiceTest  {
 	private static String BOOK_TITLE = "Junit book title";
 	private static String USER_NAME = "theUserName";
 	private static String USER_EMAIL = "theUserEmail";
+	private static JobRunRequest JOB_RUN_REQUEST = JobRunRequest.create(BOOK_CODE, USER_NAME, USER_EMAIL);
 
-	@Autowired
 	private EngineServiceImpl service;
-	private JobRunRequest jobRunRequest;
 	private JobParameters databaseParams;
+	
+	private EngineDao mockDao;
+	private JobLauncher mockJobLauncher;
+	private JobOperator mockJobOperator;
+	private JobRegistry mockJobRegistry;
+	private JobExecution mockJobExecution;
+	private Job mockJob;
 	
 	@Before
 	public void setUp() throws Exception {
-		EngineDao dao = EasyMock.createMock(EngineDao.class);
-		JobLauncher jobLauncher = EasyMock.createMock(JobLauncher.class);
-		JobOperator jobOperator = EasyMock.createMock(JobOperator.class);
-		JobRegistry jobRegistry = EasyMock.createMock(JobRegistry.class);
-		JobExecution jobExecution = EasyMock.createMock(JobExecution.class);
-		Job job = EasyMock.createMock(Job.class);
 		
-		this.jobRunRequest = JobRunRequest.create(BOOK_CODE, USER_NAME, USER_EMAIL);
 		Map<String,JobParameter> paramMap = new HashMap<String,JobParameter>();
 		paramMap.put(EngineConstants.JOB_PARAM_BOOK_TITLE, new JobParameter(BOOK_TITLE));
 		paramMap.put(myIntKey, new JobParameter(myIntValue));
 		paramMap.put(myStrKey, new JobParameter(myStrValue));
 		this.databaseParams = new JobParameters(paramMap);
-		JobParameters jobParams = EngineServiceImpl.createCombinedJobParameters(jobRunRequest, databaseParams);
-				
-		EasyMock.expect(dao.loadJobParameters(EngineConstants.JOB_DEFINITION_EBOOK)).andReturn(jobParams);
-		EasyMock.replay(dao);
-		EasyMock.expect(job.getName()).andReturn(EngineConstants.JOB_DEFINITION_EBOOK);
-		EasyMock.replay(job);
-		EasyMock.expect(jobRegistry.getJob(EngineConstants.JOB_DEFINITION_EBOOK)).andReturn(job);
-		EasyMock.replay(jobRegistry);
-		EasyMock.expect(jobLauncher.run(job, jobParams)).andReturn(jobExecution);
-		EasyMock.replay(jobLauncher);
+
+		this.mockDao = EasyMock.createMock(EngineDao.class);
+		this.mockJobLauncher = EasyMock.createMock(JobLauncher.class);
+		this.mockJobOperator = EasyMock.createMock(JobOperator.class);
+		this.mockJobRegistry = EasyMock.createMock(JobRegistry.class);
+		this.mockJobExecution = EasyMock.createMock(JobExecution.class);
+		this.mockJob = EasyMock.createMock(Job.class);
 		
 		this.service = new EngineServiceImpl();
-		service.setDao(dao);
-		service.setJobLauncher(jobLauncher);
-		service.setJobOperator(jobOperator);
-		service.setJobRegistry(jobRegistry);
+		service.setDao(mockDao);
+		service.setJobLauncher(mockJobLauncher);
+		service.setJobOperator(mockJobOperator);
+		service.setJobRegistry(mockJobRegistry);
 	}
 	@Test
 	public void testCreateCombinedJobParameters() {	
-		JobParameters combinedJobParams = EngineServiceImpl.createCombinedJobParameters(jobRunRequest, databaseParams);
+		JobParameters combinedJobParams = EngineServiceImpl.createCombinedJobParameters(JOB_RUN_REQUEST, databaseParams);
 		assertEquals(myIntValue, (Long) combinedJobParams.getLong(myIntKey));
 		assertEquals(myStrValue, combinedJobParams.getString(myStrKey));
 		assertEquals(BOOK_CODE, combinedJobParams.getString(EngineConstants.JOB_PARAM_BOOK_CODE));
@@ -86,37 +81,61 @@ public class EngineServiceTest  {
 	}
 
 	@Test
-	public void testRunJob() {
+	public void testRunJob() throws Exception {
+		JobParameters jobParams = EngineServiceImpl.createCombinedJobParameters(JOB_RUN_REQUEST, databaseParams);
+
+		EasyMock.expect(mockDao.loadJobParameters(EngineConstants.JOB_DEFINITION_EBOOK)).andReturn(jobParams);
+		EasyMock.expect(mockJobLauncher.run(mockJob, jobParams)).andReturn(mockJobExecution);
+		EasyMock.expect(mockJobRegistry.getJob(EngineConstants.JOB_DEFINITION_EBOOK)).andReturn(mockJob);
+		
+		EasyMock.replay(mockDao);
+		EasyMock.replay(mockJobLauncher);
+		EasyMock.replay(mockJob);
+		EasyMock.replay(mockJobRegistry);
+		
 		try {
-			JobExecution jobExecution = service.runJob(jobRunRequest);
+			JobExecution jobExecution = service.runJob(JOB_RUN_REQUEST);
 			Assert.assertNotNull(jobExecution);
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail("Running job should not have thrown an exception");
 		}
+		EasyMock.verify(mockDao);
+		EasyMock.verify(mockJobLauncher);
+		EasyMock.verify(mockJob);
+		EasyMock.verify(mockJobRegistry);
 	}
 
 	@Test
 	public void testRestartJob() {
+
 		try {
-			long jobExecutionId = 1234;
-			service.restartJob(jobExecutionId);
+			Long jobExecutionId = new Long(1234);
+			EasyMock.expect(mockJobOperator.restart(jobExecutionId)).andReturn(jobExecutionId);
+			EasyMock.replay(mockJobOperator);
+			
+			Long restartedId = service.restartJob(jobExecutionId);
+			Assert.assertEquals(jobExecutionId, restartedId);
 			Assert.assertTrue(true); // expected
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail("Restarting job should not have thrown an exception");
 		}
+		EasyMock.verify(mockJobOperator);
 	}
 
 	@Test
 	public void testStopJob() {
 		try {
-			long jobExecutionId = 1234;
+			Long jobExecutionId = new Long(1234);
+			EasyMock.expect(mockJobOperator.stop(jobExecutionId)).andReturn(true);
+			EasyMock.replay(mockJobOperator);
+			
 			service.stopJob(jobExecutionId);
-			Assert.assertTrue(true); // expected
 		} catch (Exception e) {
 			e.printStackTrace();
 			Assert.fail("Stopping job should not have thrown an exception");
 		}
+		EasyMock.verify(mockJobOperator);
 	}
 }
