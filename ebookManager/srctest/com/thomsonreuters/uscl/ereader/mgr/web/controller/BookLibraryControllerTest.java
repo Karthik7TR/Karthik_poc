8e382247813d6e626fbd94f06d360bcd65dc4623
u0057241 <u0057241@tlr.tfs.local>
2012-02-07 20:18:59 +0000

    initial
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C191333

---
+++ ebookManager/srctest/com/thomsonreuters/uscl/ereader/mgr/web/controller/BookLibraryControllerTest.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright 2012: Thomson Reuters Global Resources. All Rights Reserved.
+ * Proprietary and Confidential information of TRGR. Disclosure, Use or
+ * Reproduction without the written authorization of TRGR is prohibited
+ */
+package com.thomsonreuters.uscl.ereader.mgr.web.controller;
+
+public class BookLibraryControllerTest {
+
+}
