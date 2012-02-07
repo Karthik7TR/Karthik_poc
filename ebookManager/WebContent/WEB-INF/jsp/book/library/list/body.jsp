f6eb39998115140a4af4a89bfea7e5c77fa049db
Kim, Dong W. (TR Tech, Content & Ops) <dong.kim@thomsonreuters.com>
2012-02-07 19:47:30 +0000

    Displaying Book definition list
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C191305

--- ebookManager/WebContent/WEB-INF/jsp/book/library/list/body.jsp
+++ ebookManager/WebContent/WEB-INF/jsp/book/library/list/body.jsp
@@ -1,8 +1,21 @@
+<!--
+	Copyright 2011: Thomson Reuters Global Resources. All Rights Reserved.
+	Proprietary and Confidential information of TRGR. Disclosure, Use or
+	Reproduction without the written authorization of TRGR is prohibited
+-->
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<%@page import="com.thomsonreuters.uscl.ereader.mgr.web.WebConstants"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %> 
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib prefix="display" uri="http://displaytag.sf.net/el" %>
+
 Book Library List Body<br>
-Line 1<br/>
-Line 2<br/>
-Line 3<br/>
-Line 4<br/>
-Line 5<br/>
-Line 6<br/>
-Line 7<br/>
+
+<%-- Table of job executions for a specific Job name --%>
+		<display:table name="books" >
+			<display:column property="bookName" title="Name" />
+			<display:column property="authorInfo" title="Auther Name" />
+		</display:table>
--- ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/web/controller/BookLibraryController.java
+++ ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/web/controller/BookLibraryController.java
@@ -1,5 +1,8 @@
 package com.thomsonreuters.uscl.ereader.mgr.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +10,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.thomsonreuters.uscl.ereader.mgr.web.WebConstants;
+import com.thomsonreuters.uscl.ereader.orchestrate.core.service.CoreService;
+import com.thomsonreuters.uscl.ereader.orchestrate.core.BookDefinition;
 
 
 @Controller
 public class BookLibraryController {
 	//private static final Logger log = Logger.getLogger(BookLibraryController.class);
 
+	private CoreService coreService;
+	
 	@RequestMapping(value=WebConstants.MVC_BOOK_LIBRARY_LIST, method = RequestMethod.GET)
 	public ModelAndView bookList(Model model) throws Exception {
-// TODO: implement this		
+
+		List<BookDefinition> books = coreService.findAllBookDefinitions();
+		
+		model.addAttribute("books", books);
+		
 		return new ModelAndView(WebConstants.VIEW_BOOK_LIBRARY_LIST);
 	}
 	
@@ -29,5 +40,10 @@ public class BookLibraryController {
 	public ModelAndView bookIcons(Model model) throws Exception {
 // TODO: implement this		
 		return new ModelAndView(WebConstants.VIEW_BOOK_LIBRARY_ICONS);
+	}
+	
+	@Required
+	public void setCoreService(CoreService service) {
+		this.coreService = service;
 	}
 }
