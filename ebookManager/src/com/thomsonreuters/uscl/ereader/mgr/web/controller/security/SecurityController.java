4f72710f6f5cb228ea8b675c8c22b0c17a0a1c9d
c114868 <c114868@tlr.tfs.local>
2012-02-15 14:56:42 +0000

    Removed debugging, added comments.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C194018

--- ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/web/controller/security/SecurityController.java
+++ ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/web/controller/security/SecurityController.java
@@ -1,10 +1,14 @@
+/*
+ * Copyright 2012: Thomson Reuters Global Resources. All Rights Reserved.
+ * Proprietary and Confidential information of TRGR. Disclosure, Use or
+ * Reproduction without the written authorization of TRGR is prohibited
+ */
 package com.thomsonreuters.uscl.ereader.mgr.web.controller.security;
 
 import java.net.URL;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,22 +18,21 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import com.thomsonreuters.uscl.ereader.mgr.web.WebConstants;
 
-
-
 @Controller
 public class SecurityController {
-	private static final Logger log = Logger.getLogger(SecurityController.class);
 	
-	/** The full URL for the Central Authentication Service (CAS) server web application, like "https://someHost/ebookCas" */
+	/**
+	 * The context root URL for the Central Authentication Service (CAS) server web application,
+	 * like "https://someHost/ebookCas"
+	 */
 	private URL casUrl;
 	
 	/**
-	 * Invoked after the j_security_check_logout URL is invoked (which invalidates the session).
-	 * This is configured in the application spring security spring bean file.
+	 * Invoked after the j_security_check_logout URL is invoked.
+	 * This is configured in the application spring security spring bean file as as logout success URL.
 	 */
 	@RequestMapping(value=WebConstants.MVC_AFTER_LOGOUT, method = RequestMethod.GET)
 	public ModelAndView afterLogout(HttpSession httpSession) throws Exception {
-		log.debug(">>>");
 		httpSession.invalidate();
 		String casLogoutUrl = casUrl + "/logout";
 		RedirectView view = new RedirectView(casLogoutUrl);
