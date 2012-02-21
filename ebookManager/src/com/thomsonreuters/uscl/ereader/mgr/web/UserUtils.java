585246d6d5e6927f17ca8f767bcc1b4dfa46b93c
u0057241 <u0057241@tlr.tfs.local>
2012-02-21 21:12:35 +0000

    refactor isSuperUser
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C195510

---
+++ ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/web/UserUtils.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2012: Thomson Reuters Global Resources. All Rights Reserved.
+ * Proprietary and Confidential information of TRGR. Disclosure, Use or
+ * Reproduction without the written authorization of TRGR is prohibited
+ */
+
+package com.thomsonreuters.uscl.ereader.mgr.web;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import com.thomsonreuters.codes.security.authentication.LdapUserInfo;
+
+public class UserUtils {
+
+	/**
+	 * Checks if the user has super user role
+	 * 
+	 * @return
+	 */
+	public static boolean isSuperUser() {
+		boolean superUser = false;
+
+		LdapUserInfo ldapUserInfo = ModelUtils.getAuthenticatedLDapUserInfo();
+
+		Collection<GrantedAuthority> authorities = ldapUserInfo
+				.getAuthorities();
+
+		for (GrantedAuthority authority : authorities) {
+			if (WebConstants.KEY_GENERATE_BUTTON_ROLE.equals(authority
+					.getAuthority())) {
+				superUser = true;
+				break;
+			}
+		}
+
+		return superUser;
+
+	}
+}
