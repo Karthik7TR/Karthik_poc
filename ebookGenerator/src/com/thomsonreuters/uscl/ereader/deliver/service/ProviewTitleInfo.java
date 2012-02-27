58cc827acb64d450a85173d1a315772d9e6f5364
u0057241 <u0057241@tlr.tfs.local>
2012-02-27 19:19:28 +0000

    Placeholder for proview title info
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C197190

---
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/deliver/service/ProviewTitleInfo.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2012: Thomson Reuters Global Resources. All Rights Reserved.
+ * Proprietary and Confidential information of TRGR. Disclosure, Use or
+ * Reproduction without the written authorization of TRGR is prohibited
+*/
+
+package com.thomsonreuters.uscl.ereader.deliver.service;
+
+/**
+ * Placeholder for proview title info
+ * @author U0057241
+ *
+ */
+public class ProviewTitleInfo {
+	
+	private String titleId;
+	private String vesrion;
+	private String publisher;
+	private String lastupdate;
+	private String status;
+	private String title;
+
+	public String getTitleId() {
+		return titleId;
+	}
+
+	public void setTitleId(String titleId) {
+		this.titleId = titleId;
+	}
+
+	public String getVesrion() {
+		return vesrion;
+	}
+
+	public void setVesrion(String vesrion) {
+		this.vesrion = vesrion;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getLastupdate() {
+		return lastupdate;
+	}
+
+	public void setLastupdate(String lastupdate) {
+		this.lastupdate = lastupdate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+
+}
