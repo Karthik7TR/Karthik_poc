ffe10ffa9caac2bc8b0b4dd4e9d159ba45ba7077
c114868 <c114868@tlr.tfs.local>
2012-03-09 16:13:59 +0000

    Defined security roles as an enum.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C200745

---
+++ ebookGeneratorCore/src/com/thomsonreuters/uscl/ereader/Security.java
@@ -0,0 +1,10 @@
+package com.thomsonreuters.uscl.ereader;
+
+public class Security {
+	
+	/**
+	 * Valid security roles for the application, as mapped from the groups.
+	 */
+	public enum SecurityRole { ROLE_DEVELOPER, ROLE_GUEST, ROLE_PUBLISHER, ROLE_SUPERUSER, ROLE_SUPPORT };
+
+}
