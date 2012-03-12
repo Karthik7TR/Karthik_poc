086b9e9361485a39aa4743a5cecb10e7578b6eeb
u0081674 <u0081674@tlr.tfs.local>
2012-03-12 19:14:09 +0000

    Added a workaround to manually set the authentication header on the HTTP request.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C201287

--- ebookGeneratorCore/src/com/thomsonreuters/uscl/ereader/deliver/rest/ProviewRequestCallback.java
+++ ebookGeneratorCore/src/com/thomsonreuters/uscl/ereader/deliver/rest/ProviewRequestCallback.java
@@ -7,6 +7,7 @@ package com.thomsonreuters.uscl.ereader.deliver.rest;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
 import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.web.client.RequestCallback;
 
@@ -16,11 +17,18 @@ import org.springframework.web.client.RequestCallback;
  *
  */
 public class ProviewRequestCallback implements RequestCallback {
+	private static final Logger LOG = Logger.getLogger(ProviewRequestCallback.class);
 	private static final String ACCEPT_HEADER = "Accept";
 	private static final String APPLICATION_XML_MIMETYPE = "application/xml";
 
 	@Override
 	public void doWithRequest(ClientHttpRequest clientHttpRequest) throws IOException {
 		clientHttpRequest.getHeaders().add(ACCEPT_HEADER, APPLICATION_XML_MIMETYPE);
+		/* 
+		 * TODO: Determine why the Authorization header is not being set/used prior to this point.
+		 * Once the root cause is identified remove this workaround. It is possible that registering a callback
+		 * with the RestTemplate prevents the underlying, concrete HttpClient headers (if any are present) to be ignored.
+		 */
+		clientHttpRequest.getHeaders().add("Authorization", "Basic cHVibGlzaGVyOmY5Ul96QnEzN2E=");
 	}
 }
