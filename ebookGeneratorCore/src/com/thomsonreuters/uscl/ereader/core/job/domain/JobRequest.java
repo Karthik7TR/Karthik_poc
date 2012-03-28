7b905962b2fb449dd869b73941daf374f24017ff
c114868 <c114868@tlr.tfs.local>
2012-03-28 19:42:24 +0000

    Removed tom specifc table mapping (oops).
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C205822

--- ebookGeneratorCore/src/com/thomsonreuters/uscl/ereader/core/job/domain/JobRequest.java
+++ ebookGeneratorCore/src/com/thomsonreuters/uscl/ereader/core/job/domain/JobRequest.java
@@ -29,7 +29,7 @@ import com.thomsonreuters.uscl.ereader.core.book.domain.BookDefinition;
 
 
 @Entity
-@Table(name = "JOB_REQUEST_TOM")
+@Table(name = "JOB_REQUEST")
 public class JobRequest implements Serializable {
 
 	@Deprecated  // Currently supporting only QUEUED jobs
