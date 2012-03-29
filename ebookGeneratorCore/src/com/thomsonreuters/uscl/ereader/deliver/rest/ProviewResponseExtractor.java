0baab1d935447a64ddb2efdf4ca433a190ab1150
u0081674 <u0081674@tlr.tfs.local>
2012-03-29 20:04:38 +0000

    Added logging of the proview response for Ray.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C206230

--- ebookGeneratorCore/src/com/thomsonreuters/uscl/ereader/deliver/rest/ProviewResponseExtractor.java
+++ ebookGeneratorCore/src/com/thomsonreuters/uscl/ereader/deliver/rest/ProviewResponseExtractor.java
@@ -8,21 +8,32 @@ package com.thomsonreuters.uscl.ereader.deliver.rest;
 import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.ResponseExtractor;
 
 /**
- * This class returns the response body from ProView as a unicode string.
+ * A ResponseExtractor for ProView responses.  Returns the response body as a String to be interpreted by the caller.
  * 
  * @author <a href="mailto:christopher.schwartz@thomsonreuters.com">Chris Schwartz</a>u0081674
  *
  */
 public class ProviewResponseExtractor implements ResponseExtractor<String> {
-
+	private static final Logger LOG = Logger.getLogger(ProviewResponseExtractor.class);
+	
+	/**
+	 * Logs the ProView HTTP response for publishing operations and returns the response body as a unicode string.
+	 */
 	@Override
-	public String extractData(ClientHttpResponse response)
-			throws IOException {
-		return IOUtils.toString(response.getBody(), "UTF-8");
+	public String extractData(ClientHttpResponse clientHttpResponse) throws IOException {
+		String statusCode = clientHttpResponse.getStatusCode().toString();
+		String statusPhrase = clientHttpResponse.getStatusCode().getReasonPhrase();
+		String responseBody = IOUtils.toString(clientHttpResponse.getBody(), "UTF-8");
+		
+		LOG.debug("HTTP HEADERS: " + clientHttpResponse.getHeaders().toString());
+		LOG.debug("HTTP STATUS: " + statusCode + ", Reason: " + statusPhrase);
+		LOG.debug("HTTP BODY: " + responseBody);
+		
+		return responseBody;
 	}
-
 }
