4474446bc09924a5d2e2efd3d7512b94e7e53a89
u0057241 <u0057241@tlr.tfs.local>
2012-04-05 20:09:56 +0000

    dont sort by publisheddate
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C208285

--- ebookGeneratorCore/src/com/thomsonreuters/uscl/ereader/core/book/dao/BookDefinitionDaoImpl.java
+++ ebookGeneratorCore/src/com/thomsonreuters/uscl/ereader/core/book/dao/BookDefinitionDaoImpl.java
@@ -20,99 +20,118 @@ import com.thomsonreuters.uscl.ereader.core.book.domain.BookDefinition;
 import com.thomsonreuters.uscl.ereader.core.book.domain.PublisherCode;
 
 public class BookDefinitionDaoImpl implements BookDefinitionDao {
-	//private static final Logger log = Logger.getLogger(BookDefinitionDaoImpl.class);
+	// private static final Logger log =
+	// Logger.getLogger(BookDefinitionDaoImpl.class);
 	private SessionFactory sessionFactory;
-	
+
 	public BookDefinitionDaoImpl(SessionFactory sessFactory) {
 		this.sessionFactory = sessFactory;
 	}
-	
+
 	@Override
 	public BookDefinition findBookDefinitionByTitle(String titleId) {
-		BookDefinition bookDef = (BookDefinition) sessionFactory.getCurrentSession().createCriteria(BookDefinition.class)
-		 .add( Restrictions.eq("fullyQualifiedTitleId", titleId)).uniqueResult();
+		BookDefinition bookDef = (BookDefinition) sessionFactory
+				.getCurrentSession().createCriteria(BookDefinition.class)
+				.add(Restrictions.eq("fullyQualifiedTitleId", titleId))
+				.uniqueResult();
 
 		return bookDef;
 	}
-	
+
 	@Override
 	public BookDefinition findBookDefinitionByEbookDefId(Long ebookDefId) {
-		return (BookDefinition) sessionFactory.getCurrentSession().get(BookDefinition.class, ebookDefId);
+		return (BookDefinition) sessionFactory.getCurrentSession().get(
+				BookDefinition.class, ebookDefId);
 	}
-	
+
 	@Override
 	@Deprecated
 	@SuppressWarnings("unchecked")
 	public List<BookDefinition> findAllBookDefinitions() {
-		String namedQuery = "findBookDefnBySearchCriterion";	
-		String bookDefnQuery = sessionFactory.getCurrentSession().getNamedQuery(namedQuery).getQueryString();
-		Query query = sessionFactory.getCurrentSession().createQuery(bookDefnQuery);				
+		String namedQuery = "findBookDefnBySearchCriterion";
+		String bookDefnQuery = sessionFactory.getCurrentSession()
+				.getNamedQuery(namedQuery).getQueryString();
+		Query query = sessionFactory.getCurrentSession().createQuery(
+				bookDefnQuery);
 		return query.list();
 	}
 
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<BookDefinition> findBookDefinitions(String sortProperty, boolean isAscending, int pageNumber, int itemsPerPage) {
-		String namedQuery = "findBookDefnBySearchCriterion";	
-		String bookDefnQuery = sessionFactory.getCurrentSession().getNamedQuery(namedQuery).getQueryString() + "order by " + sortProperty;
-		bookDefnQuery += (isAscending) ? " asc" : " desc";	
-		Query query = sessionFactory.getCurrentSession().createQuery(bookDefnQuery);
-		query.setFirstResult((pageNumber-1)*(itemsPerPage));
+	public List<BookDefinition> findBookDefinitions(String sortProperty,
+			boolean isAscending, int pageNumber, int itemsPerPage) {
+		String namedQuery = "findBookDefnBySearchCriterion";
+		String bookDefnQuery = sessionFactory.getCurrentSession()
+				.getNamedQuery(namedQuery).getQueryString()
+				+ "order by " + sortProperty;
+		bookDefnQuery += (isAscending) ? " asc" : " desc";
+		Query query = sessionFactory.getCurrentSession().createQuery(
+				bookDefnQuery);
+		query.setFirstResult((pageNumber - 1) * (itemsPerPage));
 		query.setMaxResults(itemsPerPage);
 		return query.list();
 	}
-	
+
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<BookDefinition> findBookDefinitions(String sortProperty, boolean isAscending, int pageNumber, int itemsPerPage, String proviewDisplayName, String fullyQualifiedTitleId, String isbn,
-			String materialId, String to, String from, String status) {
-		
+	public List<BookDefinition> findBookDefinitions(String sortProperty,
+			boolean isAscending, int pageNumber, int itemsPerPage,
+			String proviewDisplayName, String fullyQualifiedTitleId,
+			String isbn, String materialId, String to, String from,
+			String status) {
+
 		HibernateTemplate ht = new HibernateTemplate(sessionFactory);
-		
-		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(BookDefinition.class);
-		criteria.setMaxResults(10);
-		
-		
-//		criteria.addOrder(isAscending?Order.asc(proviewDisplayName):Order.desc(proviewDisplayName));
-//		
-//		if (proviewDisplayName!=null && !proviewDisplayName.equals("")){
-//			criteria.add(Restrictions.eq("proviewDisplayName", proviewDisplayName));
-//		}
-//		
-//		if (fullyQualifiedTitleId!=null && !fullyQualifiedTitleId.equals("")){
-//			criteria.add(Restrictions.eq("fullyQualifiedTitleId", fullyQualifiedTitleId));
-//		}
-//		
-//		if (isbn!=null && !isbn.equals("")){
-//			criteria.add(Restrictions.eq("isbn", isbn));
-//		}
-//		
-//		if (materialId!=null && !materialId.equals("")){
-//			criteria.add(Restrictions.eq("materialId", materialId));
-//		}
-//		
-//		if (to!=null && !to.equals("")){
-//			criteria.add(Restrictions.between("lastUpdated", from, to));
-//		}
-//		
-//		if (status!=null && !status.equals("")){
-//			criteria.add(Restrictions.eq("ebookDefinitionCompleteFlag", status));
-//		}
-		
-		
+
+		Criteria criteria = sessionFactory.getCurrentSession().createCriteria(
+				BookDefinition.class);
+
+		if (!sortProperty.equals("publishEndTimestamp")) {
+			criteria.addOrder(isAscending ? Order.asc(sortProperty) : Order
+					.desc(sortProperty));
+		}
+		//
+		// if (proviewDisplayName!=null && !proviewDisplayName.equals("")){
+		// criteria.add(Restrictions.eq("proviewDisplayName",
+		// proviewDisplayName));
+		// }
+		//
+		// if (fullyQualifiedTitleId!=null &&
+		// !fullyQualifiedTitleId.equals("")){
+		// criteria.add(Restrictions.eq("fullyQualifiedTitleId",
+		// fullyQualifiedTitleId));
+		// }
+		//
+		// if (isbn!=null && !isbn.equals("")){
+		// criteria.add(Restrictions.eq("isbn", isbn));
+		// }
+		//
+		// if (materialId!=null && !materialId.equals("")){
+		// criteria.add(Restrictions.eq("materialId", materialId));
+		// }
+		//
+		// if (to!=null && !to.equals("")){
+		// criteria.add(Restrictions.between("lastUpdated", from, to));
+		// }
+		//
+		// if (status!=null && !status.equals("")){
+		// criteria.add(Restrictions.eq("ebookDefinitionCompleteFlag", status));
+		// }
+
+		criteria.setResultTransformer(Criteria.DISTINCT_ROOT_ENTITY);
 		return criteria.list();
-		
+
 	}
 
 	@Override
 	public long countNumberOfBookDefinitions() {
-		Query query = sessionFactory.getCurrentSession().getNamedQuery("countBookDefinitions");
-		return (Long)query.uniqueResult();
+		Query query = sessionFactory.getCurrentSession().getNamedQuery(
+				"countBookDefinitions");
+		return (Long) query.uniqueResult();
 	}
 
 	@Override
 	public void removeBookDefinition(String titleId) {
-		
+
 		Session session = sessionFactory.getCurrentSession();
 		session.delete(findBookDefinitionByTitle(titleId));
 		session.flush();
@@ -121,22 +140,24 @@ public class BookDefinitionDaoImpl implements BookDefinitionDao {
 	@Override
 	public BookDefinition saveBookDefinition(BookDefinition eBook) {
 		Session session = sessionFactory.getCurrentSession();
-		
+
 		eBook.setLastUpdated(new Date());
-		
+
 		// Attach Publisher Code
-		eBook.setPublisherCodes( (PublisherCode) session.createCriteria(PublisherCode.class)
-		 .add( Restrictions.eq("name", eBook.getPublisherCodes().getName())).uniqueResult());
-		
+		eBook.setPublisherCodes((PublisherCode) session
+				.createCriteria(PublisherCode.class)
+				.add(Restrictions.eq("name", eBook.getPublisherCodes()
+						.getName())).uniqueResult());
+
 		// Save if book is new
-		if(eBook.getEbookDefinitionId() == null) {
+		if (eBook.getEbookDefinitionId() == null) {
 			session.save(eBook);
-		} 
-		
+		}
+
 		// attach child objects to book definition
 		eBook = (BookDefinition) session.merge(eBook);
 		session.flush();
-		
+
 		return eBook;
 	}
 }
