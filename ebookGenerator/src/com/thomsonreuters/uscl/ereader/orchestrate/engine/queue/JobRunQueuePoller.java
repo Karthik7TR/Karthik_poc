97e3a3eea81b2981918c00e101c79615b960cb93
Chatterjee, Nirupam (TR Tech, Content & Ops) <nirupam.chatterjee@thomsonreuters.com>
2012-04-09 13:07:06 +0000

    Clean up white space, remove unused imports and format
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C208771

--- ebookGenerator/src/com/thomsonreuters/uscl/ereader/orchestrate/engine/queue/JobRunQueuePoller.java
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/orchestrate/engine/queue/JobRunQueuePoller.java
@@ -14,79 +14,74 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.scheduling.annotation.Scheduled;
 
-import com.thomsonreuters.uscl.ereader.core.book.domain.BookDefinition;
 import com.thomsonreuters.uscl.ereader.core.job.domain.JobRequest;
 import com.thomsonreuters.uscl.ereader.core.job.service.JobRequestService;
 import com.thomsonreuters.uscl.ereader.orchestrate.engine.service.EngineService;
 import com.thomsonreuters.uscl.ereader.orchestrate.engine.service.JobStartupThrottleService;
 
 /**
- * A regularly scheduled task to check the batch job run request queue(s) for new job run request messages.
- * If a message is present, and we can run the job because less than
- * the maximum number of concurrent batch jobs is running (not throttled) then the specified job will be run.
+ * A regularly scheduled task to check the batch job run request queue(s) for
+ * new job run request messages. If a message is present, and we can run the job
+ * because less than the maximum number of concurrent batch jobs is running (not
+ * throttled) then the specified job will be run.
  */
 public class JobRunQueuePoller {
 	private static final Logger log = Logger.getLogger(JobRunQueuePoller.class);
 
-//	private BookDefinitionService bookDefinitionService;
 	private EngineService engineService;
 	private JobStartupThrottleService jobStartupThrottleService;
 	private JobRequestService jobRequestService;
-	
 
-
-
-
-	@Scheduled(fixedRate=15000)
+	@Scheduled(fixedRate = 15000)
 	public void run() {
 		try {
-				
-				// check if number of jobs running are bellow throttl limit.
-				if (jobStartupThrottleService.checkIfnewJobCanbeLaunched()) {
-					// then look to see if there is a job run request sitting on the high priority queue
-					// if there was a job to run, then launch it
 
-					JobRequest jobRequest =  jobRequestService.getNextJobToExecute();
-					
-					
-					if (jobRequest != null) {
-						// Create the dynamic set of launch parameters, things like user name, user email, and a unique serial number
-						JobParameters dynamicJobParameters = engineService
-								.createDynamicJobParameters(jobRequest);
-						// Put the dynamic job parameters in the map.
-						Map<String, JobParameter> allJobParametersMap = new HashMap<String, JobParameter>(
-								dynamicJobParameters.getParameters());
-						JobParameters allJobParameters = new JobParameters(
-								allJobParametersMap);
-						// Start the job that builds the ebook
-						log.debug(jobRequest);
-						engineService.runJob(jobRequest.JOB_NAME_CREATE_EBOOK,
-								allJobParameters);
-						jobRequestService.deleteJobRequest(jobRequest.getPrimaryKey());
-					}
+			// check if number of jobs running are bellow throttl limit.
+			if (jobStartupThrottleService.checkIfnewJobCanbeLaunched()) {
+				// then look to see if there is a job run request sitting on the
+				// high priority queue
+				// if there was a job to run, then launch it
+
+				JobRequest jobRequest = jobRequestService.getNextJobToExecute();
+				if (jobRequest != null) {
+					// Create the dynamic set of launch parameters, things like
+					// user name, user email, and a unique serial number
+					JobParameters dynamicJobParameters = engineService
+							.createDynamicJobParameters(jobRequest);
+					// Put the dynamic job parameters in the map.
+					Map<String, JobParameter> allJobParametersMap = new HashMap<String, JobParameter>(
+							dynamicJobParameters.getParameters());
+					JobParameters allJobParameters = new JobParameters(
+							allJobParametersMap);
+					// Start the job that builds the ebook
+					log.debug(jobRequest);
+					engineService.runJob(jobRequest.JOB_NAME_CREATE_EBOOK,
+							allJobParameters);
+					jobRequestService.deleteJobRequest(jobRequest
+							.getPrimaryKey());
 				}
+			}
 		} catch (Exception e) {
-			
-			log.error(String.format("Failed to fetch job run request from launch input queue.\n\t%s", e.getMessage()));
+
+			log.error(String
+					.format("Failed to fetch job run request from launch input queue.\n\t%s",
+							e.getMessage()));
 			e.printStackTrace();
 		}
 	}
-//	@Required
-//	public void setBookDefinitionService(BookDefinitionService bookDefinitionService) {
-//		this.bookDefinitionService = bookDefinitionService;
-//	}
+
 	@Required
 	public void setEngineService(EngineService engineService) {
 		this.engineService = engineService;
 	}
 
-	
 	@Required
 	public void setJobStartupThrottleService(
 			JobStartupThrottleService jobStartupThrottleService) {
 		this.jobStartupThrottleService = jobStartupThrottleService;
 	}
-	@Required 
+
+	@Required
 	public void setJobRequestService(JobRequestService jobRequestService) {
 		this.jobRequestService = jobRequestService;
 	}
