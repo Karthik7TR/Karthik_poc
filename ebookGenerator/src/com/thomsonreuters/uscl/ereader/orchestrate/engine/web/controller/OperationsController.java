de394a295ad4c0eafd37760b3b5a0c0eb02348d4
c114868 <c114868@tlr.tfs.local>
2012-04-30 18:16:41 +0000

    Do not allow a job to restart if already at max number of concurrent jobs.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C215541

--- ebookGenerator/src/com/thomsonreuters/uscl/ereader/orchestrate/engine/web/controller/OperationsController.java
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/orchestrate/engine/web/controller/OperationsController.java
@@ -10,6 +10,7 @@ import org.springframework.batch.core.launch.JobExecutionNotRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,9 @@ public class OperationsController {
 	private static final Logger log = Logger.getLogger(OperationsController.class);
 	private EngineService engineService;
 	private MessageSourceAccessor messageSourceAccessor;
+	private ThreadPoolTaskExecutor springBatchTaskExecutor;
+	/** Maximum number of jobs allowed to run concurrently */
+	private int maximumConcurrentJobs;
 
 	/**
 	 * Handle REST request to restart an currently stopped or failed job.
@@ -41,17 +45,26 @@ public class OperationsController {
 		Long jobExecutionIdToRestart = jobExecutionId;
 		log.debug("jobExecutionIdToRestart="+jobExecutionIdToRestart);
 		JobOperationResponse opResponse = null;
-		try {
-			Long restartedJobExecutionId = engineService.restartJob(jobExecutionIdToRestart);
-			opResponse = new JobOperationResponse(restartedJobExecutionId);
-		} catch (JobInstanceAlreadyCompleteException e) {  // Cannot restart a job that is already finished
-			Object[] args = { jobExecutionIdToRestart.toString() };
-			String errorMessage = messageSourceAccessor.getMessage("err.job.instance.already.complete", args);
-			log.debug(errorMessage);
+
+		// Do not restart the job if the max number of concurrent jobs are already running
+		if (springBatchTaskExecutor.getActiveCount() < maximumConcurrentJobs) {
+			try {
+				Long restartedJobExecutionId = engineService.restartJob(jobExecutionIdToRestart);
+				opResponse = new JobOperationResponse(restartedJobExecutionId);
+			} catch (JobInstanceAlreadyCompleteException e) {  // Cannot restart a job that is already finished
+				Object[] args = { jobExecutionIdToRestart.toString() };
+				String errorMessage = messageSourceAccessor.getMessage("err.job.instance.already.complete", args);
+				log.debug(errorMessage);
+				opResponse = new JobOperationResponse(jobExecutionIdToRestart, false, errorMessage);
+			} catch (Exception e) {
+				log.debug("Job RESTART exception: " + e);
+				opResponse = new JobOperationResponse(jobExecutionIdToRestart, false, e.getMessage());
+			}
+		} else {
+			Object[] args = { jobExecutionIdToRestart.toString(), maximumConcurrentJobs }; 
+			String errorMessage = messageSourceAccessor.getMessage("err.restart.max.concurrent", args);
+			log.warn(errorMessage);
 			opResponse = new JobOperationResponse(jobExecutionIdToRestart, false, errorMessage);
-		} catch (Exception e) {
-			log.debug("Job RESTART exception: " + e);
-			opResponse = new JobOperationResponse(jobExecutionIdToRestart, false, e.getMessage());
 		}
 		model.addAttribute(WebConstants.KEY_JOB_OPERATION_RESPONSE, opResponse);
 		return new ModelAndView(WebConstants.VIEW_JOB_OPERATION_RESPONSE);
@@ -91,5 +104,13 @@ public class OperationsController {
 	@Required
 	public void setMessageSourceAccessor(MessageSourceAccessor accessor) {
 		this.messageSourceAccessor = accessor;
+	}
+	@Required
+	public void setSpringBatchTaskExecutor(ThreadPoolTaskExecutor taskExecutor) {
+		this.springBatchTaskExecutor = taskExecutor;
+	}
+	@Required
+	public void setMaximumConcurrentJobs(int maxJobs) {
+		this.maximumConcurrentJobs = maxJobs;
 	}
 }
