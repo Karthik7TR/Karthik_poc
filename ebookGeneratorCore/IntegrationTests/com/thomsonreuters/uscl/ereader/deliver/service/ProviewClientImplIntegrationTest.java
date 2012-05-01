4c17acf4a38e24135ee261b96fc0558e79d741b7
u0057241 <u0057241@tlr.tfs.local>
2012-05-01 21:20:34 +0000

    added remove/delete test
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C216147

--- ebookGeneratorCore/IntegrationTests/com/thomsonreuters/uscl/ereader/deliver/service/ProviewClientImplIntegrationTest.java
+++ ebookGeneratorCore/IntegrationTests/com/thomsonreuters/uscl/ereader/deliver/service/ProviewClientImplIntegrationTest.java
@@ -1,8 +1,8 @@
 /*
-* Copyright 2011: Thomson Reuters Global Resources. All Rights Reserved.
-* Proprietary and Confidential information of TRGR. Disclosure, Use or
-* Reproduction without the written authorization of TRGR is prohibited
-*/
+ * Copyright 2011: Thomson Reuters Global Resources. All Rights Reserved.
+ * Proprietary and Confidential information of TRGR. Disclosure, Use or
+ * Reproduction without the written authorization of TRGR is prohibited
+ */
 package com.thomsonreuters.uscl.ereader.deliver.service;
 
 import static org.junit.Assert.assertTrue;
@@ -16,6 +16,7 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -27,106 +28,154 @@ import com.thomsonreuters.uscl.ereader.deliver.rest.ProviewRequestCallbackFactor
 import com.thomsonreuters.uscl.ereader.deliver.rest.ProviewResponseExtractorFactory;
 
 /**
- * Integration tests for ProviewClientImpl. These do not get run during CI builds.
+ * Integration tests for ProviewClientImpl. These do not get run during CI
+ * builds.
  * 
- * @author <a href="mailto:christopher.schwartz@thomsonreuters.com">Chris Schwartz</a> u0081674
+ * @author <a href="mailto:christopher.schwartz@thomsonreuters.com">Chris
+ *         Schwartz</a> u0081674
  */
-public class ProviewClientImplIntegrationTest 
-{
-	private static final Logger LOG = Logger.getLogger(ProviewClientImplIntegrationTest.class);
-	
-	private static final String PROVIEW_DOMAIN_PREFIX = "proviewpublishing.int.qed.thomsonreuters.com";
+public class ProviewClientImplIntegrationTest {
+	private static final Logger LOG = Logger
+			.getLogger(ProviewClientImplIntegrationTest.class);
+
+	private static final String PROVIEW_DOMAIN_PREFIX = "proviewpublishing.int.ci.thomsonreuters.com";
 	private String getTitlesUriTemplate = "/v1/titles/uscl/all";
 	private String publishTitleUriTemplate = "/v1/title/{titleId}/{eBookVersionNumber}";
+	private String removeTitleUriTemplate = "/v1/title/{titleId}/{eBookVersionNumber}/status/removed";
+	private String deleteTitleUriTemplate = "/v1/title/{titleId}/{eBookVersionNumber}";
 	private String validateTitleUriTemplate = "";
 
 	private static final String PROVIEW_USERNAME = "publisher";
 	private static final String PROVIEW_PASSWORD = "f9R_zBq37a";
-	
+
 	private static final String PROVIEW_INVALID_USERNAME = "YARR";
 	private static final String PROVIEW_INVALID_PASSWORD = "PIRATES!";
-	
+
 	private ProviewClientImpl proviewClient;
 	private DefaultHttpClient defaultHttpClient;
 	private ProviewRequestCallbackFactory proviewRequestCallbackFactory;
 	private ProviewResponseExtractorFactory proviewResponseExtractorFactory;
-	
+
 	@Before
-	public void setUp() throws Exception
-	{
+	public void setUp() throws Exception {
 		this.proviewClient = new ProviewClientImpl();
 		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-		
+
 		defaultHttpClient = new DefaultHttpClient();
 		defaultHttpClient.getCredentialsProvider().setCredentials(
 				new AuthScope(PROVIEW_DOMAIN_PREFIX, AuthScope.ANY_PORT),
-				new UsernamePasswordCredentials(PROVIEW_USERNAME, PROVIEW_PASSWORD));
+				new UsernamePasswordCredentials(PROVIEW_USERNAME,
+						PROVIEW_PASSWORD));
 		requestFactory.setHttpClient(defaultHttpClient);
 
 		RestTemplate restTemplate = new RestTemplate(requestFactory);
-		restTemplate.getMessageConverters().add(new ProviewMessageConverter<File>());
+		restTemplate.getMessageConverters().add(
+				new ProviewMessageConverter<File>());
 		restTemplate.setErrorHandler(new ProviewHttpResponseErrorHandler());
 		proviewClient.setRestTemplate(restTemplate);
 		proviewRequestCallbackFactory = new ProviewRequestCallbackFactory();
 		proviewResponseExtractorFactory = new ProviewResponseExtractorFactory();
-		proviewClient.setProviewRequestCallbackFactory(proviewRequestCallbackFactory);
-		proviewClient.setProviewResponseExtractorFactory(proviewResponseExtractorFactory);
+		proviewClient
+				.setProviewRequestCallbackFactory(proviewRequestCallbackFactory);
+		proviewClient
+				.setProviewResponseExtractorFactory(proviewResponseExtractorFactory);
 	}
 
 	@After
-	public void tearDown() throws Exception
-	{
-		
+	public void tearDown() throws Exception {
+
 	}
-	
+
 	@Test
 	public void testGetAllTitlesHappyPath() throws Exception {
-		proviewClient.setGetTitlesUriTemplate("http://" + PROVIEW_DOMAIN_PREFIX + getTitlesUriTemplate);
+		proviewClient.setGetTitlesUriTemplate("http://" + PROVIEW_DOMAIN_PREFIX
+				+ getTitlesUriTemplate);
 		String publisherInformation = proviewClient.getAllPublishedTitles();
 		System.out.println(publisherInformation);
-		assertTrue(publisherInformation.startsWith("<titles apiversion=\"v1\" publisher=\"uscl\""));
+		assertTrue(publisherInformation
+				.startsWith("<titles apiversion=\"v1\" publisher=\"uscl\""));
 	}
-	
+
 	@Test
-	public void testPublishBookFailsBecauseItAlreadyExistsOnProview() throws Exception {
-		proviewClient.setPublishTitleUriTemplate("http://" + PROVIEW_DOMAIN_PREFIX + publishTitleUriTemplate);
+	public void testRemoveTitle() {
+		proviewClient.setRemoveTitleUriTemplate("http://"
+				+ PROVIEW_DOMAIN_PREFIX + removeTitleUriTemplate);
+		String publisherInformation = null;
+		try {
+			publisherInformation = proviewClient.removeTitle("uscl/an/blm",
+					"v1.1");
+
+		} catch (Exception e) {
+			if (e.getMessage().contains("Collection doesn't exist")) {
+				fail("Expected an exception as title doesn't exist on ProView!");
+			}
+		}
+
+		System.out.println(publisherInformation);
+	}
+
+	@Test
+	public void testDeleteTitle() {
+		String publisherInformation = null;
+		proviewClient.setDeleteTitleUriTemplate("http://"
+				+ PROVIEW_DOMAIN_PREFIX + deleteTitleUriTemplate);
+		try {
+			publisherInformation = proviewClient.deleteTitle("uscl/an/blm",
+					"v1.0");
+		} catch (Exception e) {
+			if (e.getMessage().contains("Collection doesn't exist")) {
+				fail("Expected an exception as title doesn't exist on ProView!");
+			}
+
+		}
+
+		System.out.println(publisherInformation);
+	}
+
+	@Test
+	public void testPublishBookFailsBecauseItAlreadyExistsOnProview()
+			throws Exception {
+		proviewClient.setPublishTitleUriTemplate("http://"
+				+ PROVIEW_DOMAIN_PREFIX + publishTitleUriTemplate);
 		String integrationTestTitleId = "uscl/cr/generator_integration_test";
 		String eBookVersionNumber = "v2";
 
 		File eBookDirectory = new File("/nas/ebookbuilder/data/");
-		File eBook = new File(eBookDirectory, "proview_client_integration_test.gz");
+		File eBook = new File(eBookDirectory,
+				"proview_client_integration_test.gz");
 		try {
-			proviewClient.publishTitle(integrationTestTitleId, eBookVersionNumber, eBook);
+			proviewClient.publishTitle(integrationTestTitleId,
+					eBookVersionNumber, eBook);
 			fail("Expected an exception related to the title already existing on ProView!");
-		}
-		catch (ProviewRuntimeException e) {
-			//expected
+		} catch (ProviewRuntimeException e) {
+			// expected
 		}
 	}
-	
+
 	@Test
 	public void testGetAllTitlesFailsDueToInvalidCredetials() throws Exception {
-		
+
 		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-		
+
 		defaultHttpClient = new DefaultHttpClient();
 		defaultHttpClient.getCredentialsProvider().setCredentials(
 				new AuthScope(PROVIEW_DOMAIN_PREFIX, AuthScope.ANY_PORT),
-				new UsernamePasswordCredentials(PROVIEW_INVALID_USERNAME, PROVIEW_INVALID_PASSWORD));
+				new UsernamePasswordCredentials(PROVIEW_INVALID_USERNAME,
+						PROVIEW_INVALID_PASSWORD));
 		requestFactory.setHttpClient(defaultHttpClient);
-		
+
 		RestTemplate restTemplate = new RestTemplate(requestFactory);
 		proviewClient.setRestTemplate(restTemplate);
-		
-		proviewClient.setGetTitlesUriTemplate("http://" + PROVIEW_DOMAIN_PREFIX + getTitlesUriTemplate);
-		
+
+		proviewClient.setGetTitlesUriTemplate("http://" + PROVIEW_DOMAIN_PREFIX
+				+ getTitlesUriTemplate);
+
 		try {
 			proviewClient.getAllPublishedTitles();
-			
-		}
-		catch (Exception e) {
+
+		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			//expected
+			// expected
 		}
 	}
 }
