f6589bda09f98ab03d1f12a958bd305ffec572fe
u0105927 <u0105927@tlr.tfs.local>
2012-05-07 20:54:58 +0000

    publish stats for image retrieval positive and negative scenario.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C217813

--- ebookGenerator/src/com/thomsonreuters/uscl/ereader/gather/step/GatherImageVerticalImagesTask.java
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/gather/step/GatherImageVerticalImagesTask.java
@@ -11,6 +11,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.batch.core.ExitStatus;
@@ -22,18 +24,25 @@ import org.springframework.beans.factory.annotation.Required;
 import org.springframework.util.Assert;
 
 import com.thomsonreuters.uscl.ereader.JobExecutionKey;
+import com.thomsonreuters.uscl.ereader.StatsUpdateTypeEnum;
 import com.thomsonreuters.uscl.ereader.core.book.domain.BookDefinition;
+import com.thomsonreuters.uscl.ereader.gather.image.domain.ImageException;
 import com.thomsonreuters.uscl.ereader.gather.image.service.ImageService;
 import com.thomsonreuters.uscl.ereader.gather.image.service.ImageServiceImpl;
 import com.thomsonreuters.uscl.ereader.orchestrate.core.tasklet.AbstractSbTasklet;
+import com.thomsonreuters.uscl.ereader.stats.domain.PublishingStats;
+import com.thomsonreuters.uscl.ereader.stats.service.PublishingStatsService;
+import org.apache.log4j.Logger;
+
 
 /**
  * Fetch book images from the Image Vertical REST web service and save them
  * into a specified image destination directory.
  */
 public class GatherImageVerticalImagesTask extends AbstractSbTasklet {
-	//private static final Logger log = Logger.getLogger(GatherImageVerticalImagesTask.class);
+	private static final Logger log = Logger.getLogger(GatherImageVerticalImagesTask.class);
 	private ImageService imageService;
+	private PublishingStatsService publishingStatsService;
 
 	@Override
 	public ExitStatus executeStep(StepContribution contribution, ChunkContext chunkContext) throws Exception {
@@ -64,11 +73,57 @@ public class GatherImageVerticalImagesTask extends AbstractSbTasklet {
 		// Fetch the image metadata and file bytes
 		long jobInstanceId = jobInstance.getId();
 		String titleId = bookDefinition.getFullyQualifiedTitleId();
-		imageService.fetchImageVerticalImages(imgDocGuidMap, dynamicImageDestinationDirectory, jobInstanceId, titleId);
+		int imageGuidNum = imgDocGuidMap.size();
+		try {
+			
+			imageService.fetchImageVerticalImages(imgDocGuidMap, dynamicImageDestinationDirectory, jobInstanceId, titleId);
+			updateImageRetrivalStats(imageGuidNum, jobInstanceId,null);
 
+			
+		} catch (ImageException e) {
+			
+			updateImageRetrivalStats(imageGuidNum, jobInstanceId,e);
+			throw e; 
+		}
+		
 		return ExitStatus.COMPLETED;
 	}
+
+
+
+	private void updateImageRetrivalStats(int imageGuidsSize,
+			long jobInstanceId,ImageException e) {
+
+		Long jobInstanceLong = jobInstanceId;
+		int retrievedCound = imageGuidsSize;
+		
+		if(null == e){
+			retrievedCound = imageGuidsSize;;
+		}else{
+			retrievedCound = getMissingGuidsCount(e);
+		}
+		
+		PublishingStats jobstatsDoc = new PublishingStats();
+		jobstatsDoc.setJobInstanceId(jobInstanceLong);
+		jobstatsDoc.setGatherImageExpectedCount(imageGuidsSize);
+		jobstatsDoc.setGatherImageRetrievedCount(retrievedCound);
+		publishingStatsService.updatePublishingStats(jobstatsDoc, StatsUpdateTypeEnum.GATHERIMAGE);
+	}
 	
+	
+	
+	private int getMissingGuidsCount(ImageException e) {
+		int missingGuidNo = 0;
+		if(e != null ){
+			String stringMessage = e.getMessage();
+			Pattern intsOnly = Pattern.compile("\\d+"); 
+			Matcher makeMatch = intsOnly.matcher(stringMessage); 
+			makeMatch.find(); 
+			String inputInt = makeMatch.group(); 
+			missingGuidNo = Integer.parseInt(inputInt);
+		}
+		return missingGuidNo;
+	}	
 	/**
 	 * Reads the contents of a text file and return each line as an element in the returned list.
 	 * The file is assumed to already exist.
@@ -104,5 +159,12 @@ public class GatherImageVerticalImagesTask extends AbstractSbTasklet {
 	@Required
 	public void setImageService(ImageService imageService) {
 		this.imageService = imageService;
+	}
+
+
+	@Required
+	public void setPublishingStatsService(
+			PublishingStatsService publishingStatsService) {
+		this.publishingStatsService = publishingStatsService;
 	}
 }
