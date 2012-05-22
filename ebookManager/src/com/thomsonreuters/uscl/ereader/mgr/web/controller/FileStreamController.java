3cd0550fafbe5a979dbbd45fa3c46e8fea9eef0b
Kim, Dong W. (TR Tech, Content & Ops) <dong.kim@thomsonreuters.com>
2012-05-22 15:13:09 +0000

    Bug fix: Cover images and Front Matter Preview CSS not streaming to page.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C222482

--- ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/web/controller/FileStreamController.java
+++ ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/web/controller/FileStreamController.java
@@ -5,13 +5,13 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.apache.log4j.Logger;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -20,32 +20,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.thomsonreuters.uscl.ereader.mgr.web.WebConstants;
 
 /**
- * Handles the web container throwing a bubbled-up application exception and displays
- * the exception stack trace on its own page.
+ * Controller used to stream files from NAS location to the web application.
  */
 @Controller
-public class ImageController {
-	// private static final Logger log = Logger.getLogger(AppExceptionController.class);
+public class FileStreamController {
+	private static final Logger log = Logger.getLogger(AppExceptionController.class);
 	
 	@RequestMapping(value=WebConstants.MVC_COVER_IMAGE, method = RequestMethod.GET)
-	public ResponseEntity<byte[]> getCoverImage(@RequestParam String imageName, HttpServletRequest request) {
+	public void getCoverImage(@RequestParam String imageName, HttpServletRequest request, HttpServletResponse response) {
 
-		return retrieveFile(request, WebConstants.LOCATION_KEY_COVER_IMAGE, imageName, MediaType.IMAGE_PNG);
+		retrieveFile(request, response, WebConstants.LOCATION_KEY_COVER_IMAGE, imageName, MediaType.IMAGE_PNG);
 	}
 	
 	@RequestMapping(value=WebConstants.MVC_FRONT_MATTER_IMAGE, method = RequestMethod.GET)
-	public ResponseEntity<byte[]> getFrontMatterImage(@RequestParam String imageName, HttpServletRequest request) {
+	public void getFrontMatterImage(@RequestParam String imageName, HttpServletRequest request, HttpServletResponse response) {
 		
-		return retrieveFile(request, WebConstants.LOCATION_FRONT_MATTER_IMAGE, imageName, MediaType.IMAGE_PNG);
+		retrieveFile(request, response, WebConstants.LOCATION_FRONT_MATTER_IMAGE, imageName, MediaType.IMAGE_PNG);
 	}
 	
 	@RequestMapping(value=WebConstants.MVC_FRONT_MATTER_CSS, method = RequestMethod.GET)
-	public ResponseEntity<byte[]> getFrontMatterCss(@RequestParam String cssName, HttpServletRequest request) {
+	public void getFrontMatterCss(@RequestParam String cssName, HttpServletRequest request, HttpServletResponse response) {
 
-		return retrieveFile(request, WebConstants.LOCATION_FRONT_MATTER_CSS, cssName, MediaType.TEXT_HTML);
+		retrieveFile(request, response, WebConstants.LOCATION_FRONT_MATTER_CSS, cssName, MediaType.TEXT_HTML);
 	}
 	
-	private ResponseEntity<byte[]> retrieveFile(HttpServletRequest request, String nasLocation,String filename, MediaType mediaType) {
+	private void retrieveFile(HttpServletRequest request, HttpServletResponse response, String nasLocation,String filename, MediaType mediaType) {
 		InputStream fin = null;
 		byte[] content = null;
 		
@@ -58,23 +57,25 @@ public class ImageController {
 			} else {
 				fin = new FileInputStream(file);
 			}
-			content = IOUtils.toByteArray(fin);
 
+			content = IOUtils.toByteArray(fin);
+			response.setContentType(mediaType.toString());
+			response.setContentLength(content.length);
+			
+			ServletOutputStream out = response.getOutputStream();
+			out.write(content);
+			out.flush();
 		} catch(Exception e) {
-			e.printStackTrace();
+			log.error("Error streaming file: ",e);
 		} finally {
 			try {
 				if(fin != null) {
 					fin.close();
 				}
 			} catch (Exception e) {
-				e.printStackTrace();
+				log.error("Error closing input stream: ",e);
 			}
 		}
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(mediaType);
-		return new ResponseEntity<byte[]>(content, headers, HttpStatus.OK);
-		
 	}
 
 }
