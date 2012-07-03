3e50a01ad507fe4bd3d1f927f2d3f94b3f6913b1
Chatterjee, Nirupam (TR Tech, Content & Ops) <nirupam.chatterjee@thomsonreuters.com>
2012-07-03 17:08:54 +0000

    Add data fix for internal linking
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C235570

--- ebookGenerator/resources/SQL/DML/PACE_AUTHORITY_UPDATE_SCRIPT.sql
+++ ebookGenerator/resources/SQL/DML/PACE_AUTHORITY_UPDATE_SCRIPT.sql
@@ -20,3 +20,34 @@ SET P.PUBLICATION_NAME = '1FLPRAC'
 WHERE P.STD_PUB_NAME = 'FLPRAC';
 
 COMMIT;
+
+update pace_metadata p
+set p.std_pub_name = 'FLAPRACEVIDENCE'
+where p.publication_name = '1FLPRAC';
+
+COMMIT;
+
+update pace_metadata p
+set p.publication_name = '1FLPRAC'
+where p.std_pub_name = 'FLAPRACEVIDENCE';
+
+commit;
+
+update pace_metadata p
+set p.std_pub_name='MINNPRACCRIMINALLAWPROCEDURE'
+where  p.std_pub_name='MNPRAC';
+
+UPDATE PACE_METADATA p
+SET p.publication_name = 'MNPRAC'
+where P.STD_PUB_NAME = 'MINNPRACCRIMINALLAWPROCEDURE';
+
+commit;
+
+
+update pace_metadata p
+set  p.publication_name = 'WAPRAC'
+where  P.STD_PUB_NAME = 'WASHPRACMETHODSOFPRACTICE'
+and p.publication_name = '1WAPRAC';
+
+commit;
+
