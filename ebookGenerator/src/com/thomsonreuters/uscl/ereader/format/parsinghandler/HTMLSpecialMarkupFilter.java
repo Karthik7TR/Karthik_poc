0dbd78b65c7360b87f84ff1a86bd6c5b83f50c00
u0095869 <u0095869@tlr.tfs.local>
2012-07-18 13:50:05 +0000

    Selvedin: Added markup to highlight text that was inserted into document for the Sampson and Tindall eBook in particular but the highlighting feature is available in all books.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C239526

--- ebookGenerator/src/com/thomsonreuters/uscl/ereader/format/parsinghandler/HTMLSpecialMarkupFilter.java
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/format/parsinghandler/HTMLSpecialMarkupFilter.java
@@ -19,54 +19,51 @@ import org.xml.sax.helpers.XMLFilterImpl;
  * Output xml element: <span class=”co_crosshatch”>Tell the Feds Mike drove you away from the bank.</span>
  *
  * @author <a href="mailto:nirupam.chatterjee@thomsonreuters.com">Nirupam Chatterjee</a> u0072938
+ * @author <a href="mailto:Selvedin.Alic@thomsonreuters.com">Selvedin Alic</a> u0095869
  */
-public class HTMLSpecialMarkupFilter extends XMLFilterImpl {
+public class HTMLSpecialMarkupFilter extends XMLFilterImpl 
+{
+	protected static final String DEL_TAG = "del";
+	protected static final String INS_TAG = "ins";
+	protected static final String SPAN_TAG = "span";
+	protected static final String INS_STYLE = "background-color: #ccffff; text-decoration: none;";
 	
-	private boolean isdelTag = false;
-
 	@Override
 	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException
 	{
-		if (qName.equalsIgnoreCase("del")) {
-			isdelTag = true;
+		if (qName.equalsIgnoreCase(DEL_TAG)) 
+		{
 			AttributesImpl newAtts = new AttributesImpl(atts);
 			newAtts.addAttribute("", "", "class", "CDATA", "co_crosshatch");
-			super.startElement(uri, localName, "span", newAtts);
-		} else if (!qName.equalsIgnoreCase("ins")){
+			super.startElement(uri, localName, SPAN_TAG, newAtts);
+		} 
+		else if (qName.equalsIgnoreCase(INS_TAG))
+		{
+			AttributesImpl newAtts = new AttributesImpl(atts);
+			newAtts.addAttribute("", "", "style", "CDATA", INS_STYLE);
+			super.startElement(uri, localName, SPAN_TAG, newAtts);
+		} 
+		else
+		{
 			super.startElement(uri, localName, qName, atts);
 		}
 	}
 	
 	@Override
-	public void characters(char buf[], int offset, int len) throws SAXException
-	{
-
-			super.characters(buf, offset, len);
-	}
-	
-	@Override
 	public void endElement(String uri, String localName, String qName) throws SAXException
 	{
-		//Remove any del tags and replace with span tag
-
-		if (isdelTag) 
+		//Remove any del or ins tags and replace with span tag
+		if (qName.equalsIgnoreCase(DEL_TAG)) 
 		{
-			if (qName.equalsIgnoreCase("del")) 
-			{
-				super.endElement(uri, localName, "span");
-				isdelTag = false;
-		    } 
-			else if (!qName.equalsIgnoreCase("ins"))
-			{
-			    super.endElement(uri, localName, qName);
-		    }
-		}
-		else 
+			super.endElement(uri, localName, SPAN_TAG);
+	    } 
+		else if (qName.equalsIgnoreCase(INS_TAG))
 		{
-			if (!qName.equalsIgnoreCase("ins"))
-			{
-			    super.endElement(uri, localName, qName);
-			}
-		}
+		    super.endElement(uri, localName, SPAN_TAG);
+	    } 
+		else
+		{
+		    super.endElement(uri, localName, qName);
+	    }
 	}	
 }
--- ebookGenerator/srctest/com/thomsonreuters/uscl/ereader/format/parsinghandler/HTMLSpecialMarkUpFilterTest.java
+++ ebookGenerator/srctest/com/thomsonreuters/uscl/ereader/format/parsinghandler/HTMLSpecialMarkUpFilterTest.java
@@ -34,8 +34,6 @@ public class HTMLSpecialMarkUpFilterTest {
 
 	private HTMLSpecialMarkupFilter specialMarkUpFilter;
 	private Serializer serializer;
-
-	
 	
 	@Before
 	public void setUp() throws Exception
@@ -121,7 +119,7 @@ public class HTMLSpecialMarkUpFilterTest {
 	{
 		
 		String xmlTestStr = "<del><ins>and</ins></del>";
-		String expectedResult = "<span class=\"co_crosshatch\">and</span>";
+		String expectedResult = "<span class=\"co_crosshatch\"><span style=\"background-color: #ccffff; text-decoration: none;\">and</span></span>";
 		
 		testHelper(xmlTestStr, expectedResult);
 	}
@@ -141,7 +139,7 @@ public class HTMLSpecialMarkUpFilterTest {
 	{
 		
 		String xmlTestStr = "<del><ins><name>nirupam</name>and</ins></del>";
-		String expectedResult = "<span class=\"co_crosshatch\"><name>nirupam</name>and</span>";
+		String expectedResult = "<span class=\"co_crosshatch\"><span style=\"background-color: #ccffff; text-decoration: none;\"><name>nirupam</name>and</span></span>";
 		
 		testHelper(xmlTestStr, expectedResult);
 	}
@@ -152,7 +150,7 @@ public class HTMLSpecialMarkUpFilterTest {
 		
 		String xmlTestStr = "<div id=\"co_document\" class=\"co_document co_codesStatutes\"><input alt=\"metadata\" value=\"{ &quot;citations&quot;: {  } }\" id=\"co_document_starPageMetadata\" type=\"hidden\"/><div class=\"co_documentHead\"><div class=\"co_genericBox\"><div class=\"co_genericBoxHeader\"><span/></div><div class=\"co_genericBoxContent\"><div class=\"co_genericBoxContentRight\"><a href=\"#\" class=\"co_widget_collapseIcon\"/><div class=\"co_contentBlock co_prelimBlock\">West's Revised Statutes of Nebraska Annotated <a href=\"#co_anchor_I9E486E80731511E1885FE7C411CD6DC6\" class=\"co_internalLink co_excludeAnnotations\">Currentness</a><div id=\"co_prelimContainer\"><div class=\"co_contentBlock co_prelimHead co_headtext\" id=\"co_prelimGoldenLeaf\">Rules of the District Court of the Tenth Judicial District</div></div></div></div></div><div class=\"co_genericBoxFooter\"><span/></div></div><div class=\"co_cites\">Neb Tenth Judicial District Court Rule 10-15</div><div class=\"co_title\"><div id=\"co_anchor_IA3D5A770E25511DF94BF8E0064702D09\" class=\"co_headtext\"><strong>RULE 10-15. INTERPRETERS</strong></div></div></div><!--DHE--><div class=\"co_contentBlock co_rule\" id=\"co_anchor_IA3D58060E25511DF94BF8E0064702D09\"><div class=\"co_contentBlock co_body\"><div class=\"co_paragraph\" id=\"co_anchor_IA3D5A772E25511DF94BF8E0064702D09\"><div class=\"co_paragraphText\"><ins>It is the duty of a pro se party needing an interpreter or counsel for a party needing an interpreter to notify the clerk of the district court 10 days prior to any hearing of the need for an interpreter. It shall be specified whether an interpreter is needed for one of the parties and/or one or more witnesses. It is not permissible for the parties to use a friend or relative as an interpreter. The cost for an interpreter is not assessed to the parties.</ins></div></div></div><div class=\"co_printHeading\" id=\"co_anchor_Credits\"><h2>Credits</h2></div><div class=\"co_paragraph\" id=\"co_anchor_IA3D5A773E25511DF94BF8E0064702D09\"><div class=\"co_paragraphText\">Adopted effective November 15, 2000. Amended June 30, 2010.</div></div></div>Neb Tenth Judicial District Court Rule 10-15, NE R 10 DIST Rule 10-15<table id=\"co_endOfDocument\"><tr><td>End of Document</td><td class=\"co_endOfDocCopyright\">© 2012 </td></tr></table></div>";
 		xmlTestStr = "<div class=\"co_paragraphText\"><ins>It is the duty of a pro se party needing an interpreter or counsel for a party needing an interpreter to notify the clerk of the district court 10 days prior to any hearing of the need for an interpreter. It shall be specified whether an interpreter is needed for one of the parties and/or one or more witnesses. It is not permissible for the parties to use a friend or relative as an interpreter. The cost for an interpreter is not assessed to the parties.</ins></div>";
-		String expectedResult = "<div class=\"co_paragraphText\">It is the duty of a pro se party needing an interpreter or counsel for a party needing an interpreter to notify the clerk of the district court 10 days prior to any hearing of the need for an interpreter. It shall be specified whether an interpreter is needed for one of the parties and/or one or more witnesses. It is not permissible for the parties to use a friend or relative as an interpreter. The cost for an interpreter is not assessed to the parties.</div>";
+		String expectedResult = "<div class=\"co_paragraphText\"><span style=\"background-color: #ccffff; text-decoration: none;\">It is the duty of a pro se party needing an interpreter or counsel for a party needing an interpreter to notify the clerk of the district court 10 days prior to any hearing of the need for an interpreter. It shall be specified whether an interpreter is needed for one of the parties and/or one or more witnesses. It is not permissible for the parties to use a friend or relative as an interpreter. The cost for an interpreter is not assessed to the parties.</span></div>";
 		
 		testHelper(xmlTestStr, expectedResult);
 	}
