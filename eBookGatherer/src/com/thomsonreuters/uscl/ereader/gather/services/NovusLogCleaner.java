1241f605d918863208920c4291f7193357492df8
Kim, Dong W. (TR Tech, Content & Ops) <dong.kim@thomsonreuters.com>
2012-08-24 18:08:37 +0000

    Clean up Novus Log files on server every 12 hours.
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C251518

---
+++ eBookGatherer/src/com/thomsonreuters/uscl/ereader/gather/services/NovusLogCleaner.java
@@ -0,0 +1,55 @@
+package com.thomsonreuters.uscl.ereader.gather.services;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.scheduling.annotation.Scheduled;
+
+public class NovusLogCleaner {
+	private static final Logger log = Logger.getLogger(NovusLogCleaner.class);
+	
+	private static final String NOVUS_LOG_FOLDER = "/home/asadmin/";
+	
+	/*
+	 * Clean up Novus generated Log files every 12 hours so home directory disk space does
+	 * not fill up
+	 */
+	@Scheduled(fixedRate = 12*60*60*1000)
+	public void cleanupOldNovusFiles() 
+	{
+		File dir = new File(NOVUS_LOG_FOLDER);
+		
+		FileFilter fileFilter = new FileFilter() 
+		{
+			@Override
+			public boolean accept(File dir) 
+			{
+				if(dir.isFile())
+				{
+					String name = dir.getName();
+					if(StringUtils.isNotBlank(name))
+					{
+						return name.matches("^MC-(Client|Prod).txt.\\d{2}-\\d{2}-\\d{4}$");
+					}
+				}
+				
+				return false;
+			}
+		};
+		
+		File[] files = dir.listFiles(fileFilter);
+		
+		for(File file : files)
+		{
+			try {
+				log.debug("Novus Log Clean-up: " + file.getCanonicalPath());
+				file.delete();
+			} catch (IOException e) {
+				log.debug("Novus Log Clean-up failed. " + e.getMessage());
+			}
+		}
+	}
+}
