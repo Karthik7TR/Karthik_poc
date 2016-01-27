5c564cf32bb30b06b7d683f7ed6b679e9e2572f8
u0105666 <u0105666@ten.tfs.local>
2016-01-27 14:31:26 +0000

    Converting String encoding to utf-8
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C760337

--- ebookGenerator/src/com/thomsonreuters/uscl/ereader/format/service/HTMLTransformerServiceImpl.java
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/format/service/HTMLTransformerServiceImpl.java
@@ -451,7 +451,8 @@ public class HTMLTransformerServiceImpl implements HTMLTransformerService
 				writer.write(",");
 				for (String anchors : guidAnchorEntry.getValue())
 				{
-					writer.write(anchors);
+					String convertedString = org.apache.commons.codec.binary.StringUtils.newStringUtf8(anchors.getBytes());
+					writer.write(convertedString);
 					writer.write("|");
 				}
 				writer.newLine();
