c079f696853e77173ffe7d97ee4d8f3a6c0625ce
Farrell, Zack (TR Tech, Content & Ops) <Zack.Farrell@thomsonreuters.com>
2016-08-12 22:02:01 +0000

    926089:ebook -- Rutter pubs failing with parse error
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C818605

--- ebookGenerator/src/com/thomsonreuters/uscl/ereader/deliver/step/DeliverToProview.java
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/deliver/step/DeliverToProview.java
@@ -13,7 +13,8 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang.StringUtils;
- import org.apache.log4j.LogManager; import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.StepContribution;
@@ -37,7 +38,8 @@ import com.thomsonreuters.uscl.ereader.stats.service.PublishingStatsService;
 /**
  * This class is responsible for delivering a generated eBook to ProView.
  * 
- * @author <a href="mailto:christopher.schwartz@thomsonreuters.com">Chris Schwartz</a> u0081674
+ * @author <a href="mailto:christopher.schwartz@thomsonreuters.com">Chris
+ *         Schwartz</a> u0081674
  *
  */
 public class DeliverToProview extends AbstractSbTasklet {
@@ -49,19 +51,19 @@ public class DeliverToProview extends AbstractSbTasklet {
 	private DocMetadataService docMetadataService;
 	private int maxNumberOfRetries = 2;
 	private int sleepTimeInMinutes = 15;
-	private int baseSleepTimeInMinutes=2;
-	
+	private int baseSleepTimeInMinutes = 2;
+
 	public void setSleepTimeInMinutes(int sleepTimeInMinutes) {
 		this.sleepTimeInMinutes = sleepTimeInMinutes;
 	}
-	
+
 	public void setBaseSleepTimeInMinutes(int baseSleepTimeInMinutes) {
 		this.baseSleepTimeInMinutes = baseSleepTimeInMinutes;
 	}
-    
-    public int getMaxNumberOfRetries() {
-        return this.maxNumberOfRetries;
-    }
+
+	public int getMaxNumberOfRetries() {
+		return this.maxNumberOfRetries;
+	}
 
 	public DocMetadataService getDocMetadataService() {
 		return docMetadataService;
@@ -75,93 +77,88 @@ public class DeliverToProview extends AbstractSbTasklet {
 	public ExitStatus executeStep(StepContribution contribution, ChunkContext chunkContext) throws Exception {
 		ExecutionContext jobExecutionContext = getJobExecutionContext(chunkContext);
 		JobParameters jobParameters = getJobParameters(chunkContext);
-		
+
 		Long jobInstance = chunkContext.getStepContext().getStepExecution().getJobExecution().getJobInstance().getId();
-		
-		BookDefinition bookDefinition = (BookDefinition)jobExecutionContext.get(JobExecutionKey.EBOOK_DEFINITION);		
+
+		BookDefinition bookDefinition = (BookDefinition) jobExecutionContext.get(JobExecutionKey.EBOOK_DEFINITION);
 
 		File eBook = new File(getRequiredStringProperty(jobExecutionContext, JobExecutionKey.EBOOK_FILE));
 		String fullyQualifiedTitleId = bookDefinition.getFullyQualifiedTitleId();
 		String versionNumber = VERSION_NUMBER_PREFIX + jobParameters.getString(JobParameterKey.BOOK_VERSION_SUBMITTED);
-		
+
 		long startTime = System.currentTimeMillis();
-		LOG.debug("Publishing eBook [" + fullyQualifiedTitleId+ "] to Proview.");
-		String publishStatus =  "Completed";
+		LOG.debug("Publishing eBook [" + fullyQualifiedTitleId + "] to Proview.");
+		String publishStatus = "Completed";
 		List<String> successfullyPublishisedList = new ArrayList<String>();
-		try 
-		{	
-			if(bookDefinition.isSplitBook()){
-				File workDirectory = new File(getRequiredStringProperty(jobExecutionContext, JobExecutionKey.WORK_DIRECTORY));
+		try {
+			if (bookDefinition.isSplitBook()) {
+				File workDirectory = new File(
+						getRequiredStringProperty(jobExecutionContext, JobExecutionKey.WORK_DIRECTORY));
 				if (workDirectory == null || !workDirectory.isDirectory()) {
 					throw new IOException("workDirectory must not be null and must be a directory.");
 				}
 				List<String> splitTitles = docMetadataService.findDistinctSplitTitlesByJobId(jobInstance);
 				for (String splitTitleId : splitTitles) {
-					
+
 					fullyQualifiedTitleId = splitTitleId;
 					splitTitleId = StringUtils.substringAfterLast(splitTitleId, "/");
 					eBook = new File(workDirectory, splitTitleId + JobExecutionKey.BOOK_FILE_TYPE_SUFFIX);
-					if(eBook == null || !eBook.exists()){
+					if (eBook == null || !eBook.exists()) {
 						throw new IOException("eBook cannot be null and contact ebook support for furthur analysis.");
 					}
-					proviewClient.publishTitle(fullyQualifiedTitleId, versionNumber, eBook);					
+					proviewClient.publishTitle(fullyQualifiedTitleId, versionNumber, eBook);
 					successfullyPublishisedList.add(fullyQualifiedTitleId);
 				}
-			}
-			else{
+			} else {
 				proviewClient.publishTitle(fullyQualifiedTitleId, versionNumber, eBook);
 			}
-		} 
-		catch (Exception e) 
-		{
-			publishStatus =  "Failed";
-			//Remove parts of the book when there is a failure
-			for(String splitTitle : successfullyPublishisedList ){
+		} catch (Exception e) {
+			publishStatus = "Failed";
+			// Remove parts of the book when there is a failure
+			for (String splitTitle : successfullyPublishisedList) {
 				removeGroupWithRetry(splitTitle, versionNumber);
 			}
-			throw(e);
-		}
-		finally
-		{
-		    PublishingStats jobstats = new PublishingStats();
-		    jobstats.setJobInstanceId(jobInstance);
-		    jobstats.setPublishStatus("deliverEBook : " + publishStatus);
+			throw (e);
+		} finally {
+			PublishingStats jobstats = new PublishingStats();
+			jobstats.setJobInstanceId(jobInstance);
+			jobstats.setPublishStatus("deliverEBook : " + publishStatus);
 			publishingStatsService.updatePublishingStats(jobstats, StatsUpdateTypeEnum.FINALPUBLISH);
 		}
-		
+
 		long processingTime = System.currentTimeMillis() - startTime;
 		LOG.debug("Publishing complete. Time elapsed: " + processingTime + "ms");
 
-      
 		return ExitStatus.COMPLETED;
 	}
-	
+
 	protected void removeGroupWithRetry(String splitTitle, String versionNumber) {
 		boolean retryRequest = true;
 
-		//Most of the books should finish in two minutes
-		try{
-		TimeUnit.MINUTES.sleep(baseSleepTimeInMinutes);
+		// Most of the books should finish in two minutes
+		try {
+			TimeUnit.MINUTES.sleep(baseSleepTimeInMinutes);
 		} catch (InterruptedException e) {
 			LOG.error("InterruptedException during HTTP retry", e);
-		};
-		
+		}
+		;
+
 		int retryCount = 0;
 		String errorMsg = "";
 		do {
 			try {
 				String response = proviewClient.removeTitle(splitTitle, versionNumber);
-				if(response.contains("200")){
+				if (response.contains("200")) {
 					proviewClient.deleteTitle(splitTitle, versionNumber);
 				}
 				retryRequest = false;
 			} catch (ProviewException ex) {
 				errorMsg = ex.getMessage();
-				if (errorMsg.equalsIgnoreCase(CoreConstants.TTILE_IN_QUEUE)){
-					// retry a retriable request					
+				if (errorMsg.equalsIgnoreCase(CoreConstants.TTILE_IN_QUEUE)) {
+					// retry a retriable request
 
 					LOG.warn("Retriable status received: waiting " + sleepTimeInMinutes + "minutes (retryCount: "
-							+ retryCount +")");
+							+ retryCount + ")");
 
 					retryRequest = true;
 					retryCount++;
@@ -170,24 +167,24 @@ public class DeliverToProview extends AbstractSbTasklet {
 						TimeUnit.MINUTES.sleep(sleepTimeInMinutes);
 					} catch (InterruptedException e) {
 						LOG.error("InterruptedException during HTTP retry", e);
-					};
-				}
-				else {
+					}
+					;
+				} else {
 					throw new ProviewRuntimeException(errorMsg);
 				}
 			}
 		} while (retryRequest && retryCount < getMaxNumberOfRetries());
 		if (retryRequest && retryCount == getMaxNumberOfRetries()) {
-			throw new ProviewRuntimeException(
-					"Tried 3 times to remove part of the split title. Proview might be down "
+			throw new ProviewRuntimeException("Tried 3 times to remove part of the split title. Proview might be down "
 					+ "or still in the process of loading the book. Please try again later. ");
 		}
 
 	}
-	
+
 	public void setProviewClient(ProviewClient proviewClient) {
 		this.proviewClient = proviewClient;
 	}
+
 	@Required
 	public void setPublishingStatsService(PublishingStatsService publishingStatsService) {
 		this.publishingStatsService = publishingStatsService;
--- ebookGenerator/src/com/thomsonreuters/uscl/ereader/format/parsinghandler/HTMLUnlinkInternalLinksFilter.java
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/format/parsinghandler/HTMLUnlinkInternalLinksFilter.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,173 +26,157 @@ import com.thomsonreuters.uscl.ereader.gather.metadata.domain.DocMetadata;
 /**
  * Filter that handles various Anchor "<a>" tags and transforms them as needed.
  *
- * @author <a href="mailto:Kirsten.Gunn@thomsonreuters.com">Kirsten Gunn</a> u0076257
+ * @author <a href="mailto:Kirsten.Gunn@thomsonreuters.com">Kirsten Gunn</a>
+ *         u0076257
  */
 public class HTMLUnlinkInternalLinksFilter extends XMLFilterImpl {
-	
+
 	private HashSet<String> nameAnchors;
 	private HashMap<String, HashSet<String>> targetAnchors;
 	private HashMap<String, String> anchorDupTargets;
 	private ArrayList<String> unlinkDocMetadataList;
 	private Map<String, DocMetadata> docMetadataKeyedByProViewId;
 	private DocMetadata unlinkDocMetadata;
-	private int badLinkCntr = 0;
-	private int goodAnchorCntr = 0;
+	// TRUE represents valid anchors, FALSE represents a removed anchor
+	private Stack<Boolean> openAnchors = new Stack<Boolean>();
 	private String currentGuid;
-	
+
 	private Pattern pattern = Pattern.compile("^er:#([a-zA-Z0-9_]+)/[a-zA-Z0-9_]+$");
-	
-	public String getCurrentGuid() 
-	{
+
+	public String getCurrentGuid() {
 		return currentGuid;
 	}
-	public void setCurrentGuid(String currentGuid) 
-	{
+
+	public void setCurrentGuid(String currentGuid) {
 		this.currentGuid = currentGuid;
 	}
 
-	public void setTargetAnchors(HashMap<String, HashSet<String>> targetAnchors )
-	{
+	public void setTargetAnchors(HashMap<String, HashSet<String>> targetAnchors) {
 		this.targetAnchors = targetAnchors;
 	}
-	public HashMap<String, HashSet<String>> getTargetAnchors()
-	{
+
+	public HashMap<String, HashSet<String>> getTargetAnchors() {
 		return targetAnchors;
 	}
-	
-	public void setAnchorDupTargets(HashMap<String, String> anchorDupTargets) 
-	{
+
+	public void setAnchorDupTargets(HashMap<String, String> anchorDupTargets) {
 		this.anchorDupTargets = anchorDupTargets;
 	}
-	public HashMap<String, String> getAnchorDupTargets() 
-	{
+
+	public HashMap<String, String> getAnchorDupTargets() {
 		return anchorDupTargets;
 	}
-	public ArrayList<String> getUnlinkDocMetadataList() 
-	{
+
+	public ArrayList<String> getUnlinkDocMetadataList() {
 		return unlinkDocMetadataList;
 	}
-	public void setUnlinkDocMetadataList(
-			ArrayList<String> unlinkDocMetadataList) 
-	{
+
+	public void setUnlinkDocMetadataList(ArrayList<String> unlinkDocMetadataList) {
 		this.unlinkDocMetadataList = unlinkDocMetadataList;
 	}
-	
-	public DocMetadata getUnlinkDocMetadata() 
-	{
+
+	public DocMetadata getUnlinkDocMetadata() {
 		return unlinkDocMetadata;
 	}
+
 	public Map<String, DocMetadata> getDocMetadataKeyedByProViewId() {
 		return docMetadataKeyedByProViewId;
 	}
-	public void setDocMetadataKeyedByProViewId(
-			Map<String, DocMetadata> docMetadataKeyedByProViewId) {
+
+	public void setDocMetadataKeyedByProViewId(Map<String, DocMetadata> docMetadataKeyedByProViewId) {
 		this.docMetadataKeyedByProViewId = docMetadataKeyedByProViewId;
 	}
-	public void setUnlinkDocMetadata(DocMetadata unlinkDocMetadata) 
-	{
+
+	public void setUnlinkDocMetadata(DocMetadata unlinkDocMetadata) {
 		this.unlinkDocMetadata = unlinkDocMetadata;
 	}
+
 	@Override
-	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException
-	{
-		if (qName.equalsIgnoreCase("a"))
-		{
-			if (atts != null && atts.getValue("href") != null && 
-					atts.getValue("href").startsWith(FormatConstants.PROVIEW_ASSERT_REFERENCE_PREFIX_SPLIT) && atts.getValue("href").contains("/"))
-			{
+	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+		if (qName.equalsIgnoreCase("a")) {
+			if (atts != null && atts.getValue("href") != null
+					&& atts.getValue("href").startsWith(FormatConstants.PROVIEW_ASSERT_REFERENCE_PREFIX_SPLIT)
+					&& atts.getValue("href").contains("/")) {
 				String guid = currentGuid;
-				
-				String attsHrefValue = atts.getValue("href");		
-				//hrefLink value without split title 
-				attsHrefValue = FormatConstants.PROVIEW_ASSERT_REFERENCE_PREFIX_SPLIT+StringUtils.substring(attsHrefValue,attsHrefValue.indexOf("#"))  ;
-				
-				//Get the string list after # regex '/'
+
+				String attsHrefValue = atts.getValue("href");
+				// hrefLink value without split title
+				attsHrefValue = FormatConstants.PROVIEW_ASSERT_REFERENCE_PREFIX_SPLIT
+						+ StringUtils.substring(attsHrefValue, attsHrefValue.indexOf("#"));
+
+				// Get the string list after # regex '/'
 				String guidList[] = attsHrefValue.split("/");
 
-				if (guidList.length > 1 )
-				{
+				if (guidList.length > 1) {
 					guid = guidList[0].substring(4);
 				}
 
-
-				if (targetAnchors != null)
-				{
-					nameAnchors =  targetAnchors.get(guid);
+				if (targetAnchors != null) {
+					nameAnchors = targetAnchors.get(guid);
 				}
 
-				if ( nameAnchors != null && nameAnchors.contains(attsHrefValue))
-				{
-					if	( anchorDupTargets != null && anchorDupTargets.containsKey(attsHrefValue))
-					{
-					// change href REPLACEWITH existing anchor
+				if (nameAnchors != null && nameAnchors.contains(attsHrefValue)) {
+					if (anchorDupTargets != null && anchorDupTargets.containsKey(attsHrefValue)) {
+						// change href REPLACEWITH existing anchor
 
-						for ( Entry<String, String> dupTarget : anchorDupTargets.entrySet() )
-						{
-							if (dupTarget.getKey().contains(attsHrefValue))
-							{	
-								attsHrefValue  = dupTarget.getValue();
+						for (Entry<String, String> dupTarget : anchorDupTargets.entrySet()) {
+							if (dupTarget.getKey().contains(attsHrefValue)) {
+								attsHrefValue = dupTarget.getValue();
 								if (!dupTarget.getValue().contains("_"))
 									break; // give priority to guid without _
 							}
 						}
 						AttributesImpl newAtts = new AttributesImpl(atts);
-						String newAttsHrefValue = newAtts.getValue("href");	
-						//hrefLink value without split title 
-						String splitTitle = StringUtils.substring(newAttsHrefValue,newAttsHrefValue.indexOf("er:")+3,newAttsHrefValue.indexOf("#")); 
-						newAttsHrefValue = FormatConstants.PROVIEW_ASSERT_REFERENCE_PREFIX_SPLIT+StringUtils.substring(newAttsHrefValue,newAttsHrefValue.indexOf("#"))  ;	
-						
-						if (attsHrefValue != null && newAtts.getIndex("href") >= 0 && !attsHrefValue.equals(newAttsHrefValue))
-						{
+						String newAttsHrefValue = newAtts.getValue("href");
+						// hrefLink value without split title
+						String splitTitle = StringUtils.substring(newAttsHrefValue, newAttsHrefValue.indexOf("er:") + 3,
+								newAttsHrefValue.indexOf("#"));
+						newAttsHrefValue = FormatConstants.PROVIEW_ASSERT_REFERENCE_PREFIX_SPLIT
+								+ StringUtils.substring(newAttsHrefValue, newAttsHrefValue.indexOf("#"));
+
+						if (attsHrefValue != null && newAtts.getIndex("href") >= 0
+								&& !attsHrefValue.equals(newAttsHrefValue)) {
 							int indexHrefId = newAtts.getIndex("href");
-							//Add split title to the new link if exists
-							if (splitTitle.length() > 0){
-								attsHrefValue =  FormatConstants.PROVIEW_ASSERT_REFERENCE_PREFIX_SPLIT+splitTitle+StringUtils.substring(attsHrefValue, attsHrefValue.indexOf("#"));
+							// Add split title to the new link if exists
+							if (splitTitle.length() > 0) {
+								attsHrefValue = FormatConstants.PROVIEW_ASSERT_REFERENCE_PREFIX_SPLIT + splitTitle
+										+ StringUtils.substring(attsHrefValue, attsHrefValue.indexOf("#"));
 							}
 							newAtts.setAttribute(indexHrefId, "", "", "href", "CDATA", attsHrefValue);
 						}
 						super.startElement(uri, localName, qName, newAtts);
-						goodAnchorCntr++;
+						openAnchors.push(true); // valid link denoted by TRUE
 
-					}							
-					else
-					{
+					} else {
 						// remove anchor with no target.
-						badLinkCntr++;
-						
-						//write out link information for email report
-						if (unlinkDocMetadataList == null)
-						{
+						openAnchors.push(false); // invalid link denoted by
+													// FALSE
+
+						// write out link information for email report
+						if (unlinkDocMetadataList == null) {
 							unlinkDocMetadataList = new ArrayList<String>();
 						}
-						
+
 						StringBuffer sbDocMetadata = new StringBuffer();
-						if (unlinkDocMetadata != null)
-						{
+						if (unlinkDocMetadata != null) {
 							sbDocMetadata.append(unlinkDocMetadata.getDocUuid());
-						}
-						else
-						{
+						} else {
 							sbDocMetadata.append(currentGuid);
 						}
 						sbDocMetadata.append(",");
-						if(unlinkDocMetadata.getDocFamilyUuid() != null )
-						{
+						if (unlinkDocMetadata.getDocFamilyUuid() != null) {
 							sbDocMetadata.append(unlinkDocMetadata.getDocFamilyUuid());
 						}
 						sbDocMetadata.append(",");
-						if(unlinkDocMetadata.getNormalizedFirstlineCite() != null )
-						{
+						if (unlinkDocMetadata.getNormalizedFirstlineCite() != null) {
 							sbDocMetadata.append(unlinkDocMetadata.getNormalizedFirstlineCite());
 						}
 						sbDocMetadata.append(",");
-						if(unlinkDocMetadata.getSerialNumber() != null )
-						{
+						if (unlinkDocMetadata.getSerialNumber() != null) {
 							sbDocMetadata.append(unlinkDocMetadata.getSerialNumber());
 						}
 						sbDocMetadata.append(",");
-						if(unlinkDocMetadata.getCollectionName() != null )
-						{
+						if (unlinkDocMetadata.getCollectionName() != null) {
 							sbDocMetadata.append(unlinkDocMetadata.getCollectionName());
 						}
 						sbDocMetadata.append(",");
@@ -199,90 +184,59 @@ public class HTMLUnlinkInternalLinksFilter extends XMLFilterImpl {
 						sbDocMetadata.append(link);
 
 						Matcher matcher = pattern.matcher(link);
-						if(matcher.find()) {
+						if (matcher.find()) {
 							String proViewId = matcher.group(1);
 							DocMetadata targetDocMetadata = docMetadataKeyedByProViewId.get(proViewId);
-							if(targetDocMetadata != null) {
+							if (targetDocMetadata != null) {
 								sbDocMetadata.append(",");
-								if(StringUtils.isNotBlank(targetDocMetadata.getDocUuid())) 
-								{
+								if (StringUtils.isNotBlank(targetDocMetadata.getDocUuid())) {
 									sbDocMetadata.append(targetDocMetadata.getDocUuid());
 								}
 								sbDocMetadata.append(",");
-								if(StringUtils.isNotBlank(targetDocMetadata.getDocFamilyUuid())) 
-								{
+								if (StringUtils.isNotBlank(targetDocMetadata.getDocFamilyUuid())) {
 									sbDocMetadata.append(targetDocMetadata.getDocFamilyUuid());
 								}
 								sbDocMetadata.append(",");
-								if(StringUtils.isNotBlank(targetDocMetadata.getNormalizedFirstlineCite())) 
-								{
+								if (StringUtils.isNotBlank(targetDocMetadata.getNormalizedFirstlineCite())) {
 									sbDocMetadata.append(targetDocMetadata.getNormalizedFirstlineCite());
 								}
 								sbDocMetadata.append(",");
-								if(targetDocMetadata.getSerialNumber() != null) 
-								{
+								if (targetDocMetadata.getSerialNumber() != null) {
 									sbDocMetadata.append(targetDocMetadata.getSerialNumber());
 								}
 							}
 						}
-						
+
 						unlinkDocMetadataList.add(sbDocMetadata.toString());
 					}
-				}
-				else
-				{
+				} else {
 					super.startElement(uri, localName, qName, atts);
-					goodAnchorCntr++;
+					openAnchors.push(true);
 				}
-			}
-			else
-			{
+			} else {
 				super.startElement(uri, localName, qName, atts);
-				goodAnchorCntr++;
+				openAnchors.push(true);
 			}
-		}
-		else
-		{
+		} else {
 			super.startElement(uri, localName, qName, atts);
 		}
 	}
-	
+
 	@Override
-	public void characters(char buf[], int offset, int len) throws SAXException
-	{
-			super.characters(buf, offset, len);
+	public void characters(char buf[], int offset, int len) throws SAXException {
+		super.characters(buf, offset, len);
 	}
-	
+
 	@Override
-	public void endElement(String uri, String localName, String qName) throws SAXException
-	{
-		if (qName.equalsIgnoreCase("a"))
-		{
-			if (badLinkCntr > 0) // an anchor has been removed
-			{
-				if (goodAnchorCntr == 0)
-				{
-					badLinkCntr--;
-				}
-				if (goodAnchorCntr > 0)
-				{
-					super.endElement(uri, localName, qName);
-					goodAnchorCntr--;
-				}	
-			}
-			else 
-			{
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		if (qName.equalsIgnoreCase("a")) {
+			// If the latest anchor was valid (TRUE) add the </a>
+			// if it was removed (FALSE) don't.
+			if (openAnchors.pop()) {
 				super.endElement(uri, localName, qName);
-				if (goodAnchorCntr > 0)
-				{
-					goodAnchorCntr--;
-				}
 			}
-		}
-		else
-		{
+		} else {
 			super.endElement(uri, localName, qName);
 		}
 	}
-	
 }
