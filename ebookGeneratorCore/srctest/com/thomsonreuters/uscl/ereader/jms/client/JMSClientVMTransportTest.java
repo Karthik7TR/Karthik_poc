1fd4af5fef454b3dfb7d8ad735c9035409bfa061
Vityazev, Nikolay <Nikolay.Vityazev@thomsonreuters.com>
2017-01-25 15:47:01 +0000

    974119 - Create mock service - applied solution using VM transport
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C860386

---
+++ ebookGeneratorCore/srctest/com/thomsonreuters/uscl/ereader/jms/client/JMSClientVMTransportTest.java
@@ -0,0 +1,118 @@
+package com.thomsonreuters.uscl.ereader.jms.client;
+
+import java.util.List;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.pool.PooledConnectionFactory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.jms.core.JmsTemplate;
+
+import com.thomsonreuters.uscl.ereader.jms.client.impl.JmsClientImpl;
+
+public class JMSClientVMTransportTest {
+	/* configuration for test queue */
+	private static final String BROKER_URL = "vm://localhost?broker.persistent=false";
+	private static final String QUEUE = "TEST.QUEUE";
+
+	private JmsTemplate jmsTemplate;
+	private JMSClient client;
+
+	@Before
+	public void init() {
+		client = new JmsClientImpl();
+		jmsTemplate = getJmsTemplateWithVMConnection();
+		initQueue();
+	}
+	
+	private static JmsTemplate getJmsTemplateWithVMConnection() {
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(BROKER_URL);
+		PooledConnectionFactory jmsFactory = new PooledConnectionFactory(connectionFactory);
+		
+		JmsTemplate jmsTemplate = new JmsTemplate(jmsFactory);
+		jmsTemplate.setDefaultDestinationName(QUEUE);
+		return jmsTemplate;
+	}
+
+	@Test
+	public void testSendReceive() {
+		List<String> contents = client.receiveMessages(jmsTemplate, "");
+
+		Assert.assertEquals(5, contents.size());
+		Assert.assertTrue(contents.contains("aaa"));
+		Assert.assertTrue(contents.contains("bbb"));
+		Assert.assertTrue(contents.contains("ccc"));
+		Assert.assertTrue(contents.contains("ddd"));
+		Assert.assertTrue(contents.contains("eee"));
+	}
+
+	@Test
+	public void testReceiveNext() {
+		String content = client.receiveSingleMessage(jmsTemplate, "");
+		Assert.assertEquals("aaa", content);
+		content = client.receiveSingleMessage(jmsTemplate, "");
+		Assert.assertEquals("bbb", content);
+		content = client.receiveSingleMessage(jmsTemplate, "");
+		Assert.assertEquals("ccc", content);
+		content = client.receiveSingleMessage(jmsTemplate, "");
+		Assert.assertEquals("ddd", content);
+		content = client.receiveSingleMessage(jmsTemplate, "");
+		Assert.assertEquals("eee", content);
+		content = client.receiveSingleMessage(jmsTemplate, "");
+		Assert.assertEquals(null, content);
+	}
+
+	@Test
+	public void testReceiveByKeyword() {
+		client.sendMessageToQueue(jmsTemplate, "abc", null);
+
+		List<String> contents = client.receiveMessages(jmsTemplate, "b");
+
+		Assert.assertEquals(2, contents.size());
+		Assert.assertEquals("bbb", contents.get(0));
+		Assert.assertEquals("abc", contents.get(1));
+
+		contents = client.receiveMessages(jmsTemplate, "");
+
+		Assert.assertEquals(4, contents.size());
+		Assert.assertTrue(contents.contains("aaa"));
+		Assert.assertTrue(contents.contains("ccc"));
+		Assert.assertTrue(contents.contains("ddd"));
+		Assert.assertTrue(contents.contains("eee"));
+	}
+
+	@Test
+	public void testReceiveNextByKeyword() {
+		client.sendMessageToQueue(jmsTemplate, "abc", null);
+
+		String content = client.receiveSingleMessage(jmsTemplate, "b");
+		Assert.assertEquals("bbb", content);
+		content = client.receiveSingleMessage(jmsTemplate, "b");
+		Assert.assertEquals("abc", content);
+
+		List<String> contents = client.receiveMessages(jmsTemplate, "");
+
+		Assert.assertEquals(4, contents.size());
+		Assert.assertTrue(contents.contains("aaa"));
+		Assert.assertTrue(contents.contains("ccc"));
+		Assert.assertTrue(contents.contains("ddd"));
+		Assert.assertTrue(contents.contains("eee"));
+	}
+
+	@Test
+	public void testClearQueue() {
+		client.receiveMessages(jmsTemplate, "");
+
+		String content = client.receiveSingleMessage(jmsTemplate, "");
+		Assert.assertEquals(null, content);
+	}
+
+	private void initQueue() {
+		client.sendMessageToQueue(jmsTemplate, "aaa", null);
+		client.sendMessageToQueue(jmsTemplate, "bbb", null);
+		client.sendMessageToQueue(jmsTemplate, "ccc", null);
+		client.sendMessageToQueue(jmsTemplate, "ddd", null);
+		client.sendMessageToQueue(jmsTemplate, "eee", null);
+	}
+}
