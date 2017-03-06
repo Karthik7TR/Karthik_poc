84e76d59f1e08d5c1599ca5d2afc172b22057b41
uc220946 <uc220946@ten.tfs.local>
2017-03-06 08:41:12 +0000

    XPP job: unit test for XSL transformation service
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C870868

---
+++ ebookGenerator/srctest/com/thomsonreuters/uscl/ereader/common/xslt/XslTransformationServiceImplTest.java
@@ -0,0 +1,67 @@
+package com.thomsonreuters.uscl.ereader.common.xslt;
+
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.any;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public final class XslTransformationServiceImplTest
+{
+    @InjectMocks
+    private XslTransformationServiceImpl service;
+    @Mock
+    private Transformer transformer;
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    private File input;
+    private File output;
+
+    @Before
+    public void setUp()
+    {
+        input = new File(temporaryFolder.getRoot(), "input.xsl");
+        output = new File(temporaryFolder.getRoot(), "output.xsl");
+    }
+
+    @Test
+    public void shouldThrowExceptionIfCannotTransform()
+    {
+        //given
+        thrown.expect(XslTransformationException.class);
+        //when
+        service.transform(transformer, input, output);
+        //then
+    }
+
+    @Test
+    public void shouldTransform() throws TransformerException, IOException
+    {
+        //given
+        input.createNewFile();
+        output.createNewFile();
+        //when
+        service.transform(transformer, input, output);
+        //then
+        then(transformer).should().transform(any(Source.class), any(Result.class));
+    }
+}
