245449a2c24dc07bb58611d8f05671dd4894cea0
Vityazev, Nikolay <Nikolay.Vityazev@thomsonreuters.com>
2017-03-10 09:04:43 +0000

    990048: add annotation to the xpp dynamic images step and make its scope step
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C872113

--- ebookGenerator/src/com/thomsonreuters/uscl/ereader/xpp/gather/GatherXppDynamicImagesTask.java
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/xpp/gather/GatherXppDynamicImagesTask.java
@@ -4,13 +4,19 @@ import java.io.File;
 
 import javax.annotation.Resource;
 
+import com.thomsonreuters.uscl.ereader.StatsUpdateTypeEnum;
 import com.thomsonreuters.uscl.ereader.common.filesystem.XppUnpackFileSystem;
+import com.thomsonreuters.uscl.ereader.common.notification.step.FailureNotificationType;
+import com.thomsonreuters.uscl.ereader.common.notification.step.SendFailureNotificationPolicy;
+import com.thomsonreuters.uscl.ereader.common.publishingstatus.step.SavePublishingStatusPolicy;
 import com.thomsonreuters.uscl.ereader.gather.domain.GatherImgRequest;
 import com.thomsonreuters.uscl.ereader.gather.step.GatherDynamicImagesTask;
 
 /**
  * Gather dynamic images task for XPP pathway.
  */
+@SendFailureNotificationPolicy(FailureNotificationType.GENERATOR)
+@SavePublishingStatusPolicy(StatsUpdateTypeEnum.GATHERIMAGE)
 public class GatherXppDynamicImagesTask extends GatherDynamicImagesTask
 {
     @Resource(name = "xppUnpackFileSystem")
