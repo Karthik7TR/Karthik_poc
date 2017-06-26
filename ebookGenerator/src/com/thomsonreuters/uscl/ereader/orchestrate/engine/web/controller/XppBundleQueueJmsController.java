dc2361b8a9b2ed8058425c01b35bd089f0cc60a9
Vityazev, Nikolay <Nikolay.Vityazev@thomsonreuters.com>
2017-06-26 11:02:59 +0000

    1021295 create XppBundleQueue jms message sender
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C900025

---
+++ ebookGenerator/src/com/thomsonreuters/uscl/ereader/orchestrate/engine/web/controller/XppBundleQueueJmsController.java
@@ -0,0 +1,74 @@
+package com.thomsonreuters.uscl.ereader.orchestrate.engine.web.controller;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.Date;
+
+import javax.annotation.Resource;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import com.thomsonreuters.uscl.ereader.jms.client.JMSClient;
+import com.thomsonreuters.uscl.ereader.request.domain.XppBundleArchive;
+import com.thomsonreuters.uscl.ereader.util.UuidGenerator;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Sends JMS message to queue to launch XppBundleQueuePoller for development purposes.
+ */
+@RestController
+public class XppBundleQueueJmsController
+{
+    @Resource
+    private JMSClient jmsClient;
+    @Resource
+    private JmsTemplate jmsTemplate;
+    @Resource
+    private UuidGenerator uuidGenerator;
+
+    @RequestMapping(value = "/xppbundle", method = {RequestMethod.POST, RequestMethod.GET})
+    public XppBundleArchive sendJmsMessage(@RequestParam final String materialNumber, @RequestParam final String srcFile) throws IOException, JAXBException
+    {
+        final XppBundleArchive xppBundleRequest = buildRequest(materialNumber, srcFile);
+        jmsClient.sendMessageToQueue(jmsTemplate, marshall(xppBundleRequest), null);
+        return xppBundleRequest;
+    }
+
+    private XppBundleArchive buildRequest(final String materialNumber, final String srcFile) throws IOException
+    {
+        final XppBundleArchive xppBundleRequest = new XppBundleArchive();
+        xppBundleRequest.setMessageId(uuidGenerator.generateUuid());
+        xppBundleRequest.setBundleHash(calculateHash(srcFile));
+        xppBundleRequest.setMaterialNumber(materialNumber);
+        xppBundleRequest.setDateTime(new Date());
+        xppBundleRequest.setEBookSrcPath(srcFile);
+        xppBundleRequest.setVersion("1.0");
+        return xppBundleRequest;
+    }
+
+    private String marshall(final XppBundleArchive xppBundleRequest) throws JAXBException
+    {
+        final JAXBContext jaxbContext = JAXBContext.newInstance(XppBundleArchive.class);
+        final Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        final StringWriter sw = new StringWriter();
+        jaxbMarshaller.marshal(xppBundleRequest, sw);
+        return sw.toString();
+    }
+
+    private String calculateHash(final String srcFile) throws IOException
+    {
+        try (InputStream bookStream = new FileInputStream(srcFile))
+        {
+            return DigestUtils.md5Hex(bookStream);
+        }
+    }
+
+}
---
+++ ebookGenerator/srctest/com/thomsonreuters/uscl/ereader/orchestrate/engine/web/controller/XppBundleQueueJmsControllerTest.java
@@ -0,0 +1,58 @@
+package com.thomsonreuters.uscl.ereader.orchestrate.engine.web.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.BDDMockito.given;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.bind.JAXBException;
+
+import com.thomsonreuters.uscl.ereader.jms.client.JMSClient;
+import com.thomsonreuters.uscl.ereader.request.domain.XppBundleArchive;
+import com.thomsonreuters.uscl.ereader.util.UuidGenerator;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.jms.core.JmsTemplate;
+
+@RunWith(MockitoJUnitRunner.class)
+public class XppBundleQueueJmsControllerTest
+{
+    private static final String MATERIAL_NUMBER = "11111111";
+
+    @InjectMocks
+    private XppBundleQueueJmsController xppBundleQueueJmsController;
+
+    @Mock
+    private JMSClient jmsClient;
+    @Mock
+    private JmsTemplate jmsTemplate;
+    @Mock
+    private UuidGenerator uuidGenerator;
+
+    @Before
+    public void init()
+    {
+        given(uuidGenerator.generateUuid()).willReturn("uuid");
+    }
+
+    @Test
+    public void shouldSendJmsMessage() throws IOException, JAXBException
+    {
+        final String srcFile = File.createTempFile("temp", "").getAbsolutePath();
+
+        final XppBundleArchive request = xppBundleQueueJmsController.sendJmsMessage(MATERIAL_NUMBER, srcFile);
+
+        assertEquals(MATERIAL_NUMBER, request.getMaterialNumber());
+        assertEquals(srcFile, request.getEBookSrcPath());
+        assertNotNull(request.getMessageId());
+        assertNotNull(request.getVersion());
+        assertNotNull(request.getDateTime());
+        assertNotNull(request.getBundleHash());
+    }
+}
