c88943457466488df4b2faa0c526243937bc2928
uc228424 <uc228424@ten.tfs.local>
2017-11-08 15:15:32 +0000

    1097037 Duplicate Material numbers merging
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C945532

--- ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/web/controller/bookdefinition/edit/EditBookDefinitionServiceImpl.java
+++ ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/web/controller/bookdefinition/edit/EditBookDefinitionServiceImpl.java
@@ -1,15 +1,19 @@
 package com.thomsonreuters.uscl.ereader.mgr.web.controller.bookdefinition.edit;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.time.Instant;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.thomsonreuters.uscl.ereader.core.book.domain.DocumentTypeCode;
 import com.thomsonreuters.uscl.ereader.core.book.domain.JurisTypeCode;
@@ -20,6 +24,7 @@ import com.thomsonreuters.uscl.ereader.core.book.service.CodeService;
 import com.thomsonreuters.uscl.ereader.core.book.statecode.StateCode;
 import com.thomsonreuters.uscl.ereader.core.book.statecode.StateCodeService;
 import com.thomsonreuters.uscl.ereader.mgr.web.controller.bookdefinition.edit.sap.comparsion.MaterialComponentComparatorProvider;
+import com.thomsonreuters.uscl.ereader.sap.component.Material;
 import com.thomsonreuters.uscl.ereader.sap.component.MaterialComponent;
 import com.thomsonreuters.uscl.ereader.sap.component.MaterialComponentsResponse;
 import com.thomsonreuters.uscl.ereader.sap.component.MediaLowLevelRule;
@@ -73,26 +78,12 @@ public class EditBookDefinitionServiceImpl implements EditBookDefinitionService 
 
     @Override
     public Map<String, String> getStates() {
-        final List<StateCode> codes = stateCodeService.getAllStateCodes();
-        final Map<String, String> states = new LinkedHashMap<>();
-
-        for (final StateCode code : codes) {
-            states.put(code.getName().toLowerCase(), code.getName());
-        }
-
-        return states;
+        return buildNamesMap(stateCodeService::getAllStateCodes, StateCode::getName);
     }
 
     @Override
     public Map<String, String> getJurisdictions() {
-        final List<JurisTypeCode> codes = codeService.getAllJurisTypeCodes();
-        final Map<String, String> jurisdictions = new LinkedHashMap<>();
-
-        for (final JurisTypeCode code : codes) {
-            jurisdictions.put(code.getName().toLowerCase(), code.getName());
-        }
-
-        return jurisdictions;
+        return buildNamesMap(codeService::getAllJurisTypeCodes, JurisTypeCode::getName);
     }
 
     @Override
@@ -102,26 +93,20 @@ public class EditBookDefinitionServiceImpl implements EditBookDefinitionService 
 
     @Override
     public Map<String, String> getPubTypes() {
-        final List<PubTypeCode> codes = codeService.getAllPubTypeCodes();
-        final Map<String, String> pubTypes = new LinkedHashMap<>();
-
-        for (final PubTypeCode code : codes) {
-            pubTypes.put(code.getName().toLowerCase(), code.getName());
-        }
-
-        return pubTypes;
+        return buildNamesMap(codeService::getAllPubTypeCodes, PubTypeCode::getName);
     }
 
     @Override
     public Map<String, String> getPublishers() {
-        final List<PublisherCode> codes = codeService.getAllPublisherCodes();
-        final Map<String, String> publishers = new LinkedHashMap<>();
+        return buildNamesMap(codeService::getAllPublisherCodes, PublisherCode::getName);
+    }
 
-        for (final PublisherCode code : codes) {
-            publishers.put(code.getName().toLowerCase(), code.getName());
-        }
-
-        return publishers;
+    private <T> Map<String, String> buildNamesMap(final Supplier<Collection<T>> dataSupplier,
+                                                  final Function<T, String> getNameFunction) {
+        return dataSupplier.get().stream()
+            .map(getNameFunction)
+            .collect(Collectors.toMap(
+                String::toLowerCase, Function.identity(), (oldVal, newVal) -> newVal, LinkedHashMap::new));
     }
 
     @Override
@@ -137,50 +122,62 @@ public class EditBookDefinitionServiceImpl implements EditBookDefinitionService 
     @Override
     public List<String> getCodesWorkbenchDirectory(final String folder) {
         if (StringUtils.isNotBlank(folder)) {
-            final File dir = new File(rootCodesWorkbenchLandingStrip, folder);
-            if (dir.exists()) {
-                final List<String> files = Arrays.asList(dir.list());
-                Collections.sort(files, String.CASE_INSENSITIVE_ORDER);
-                return files;
-            } else {
-                return null;
-            }
+            return Optional.of(new File(rootCodesWorkbenchLandingStrip, folder))
+                .filter(File::exists)
+                .map(dir -> collectFileNames(dir::list))
+                .orElse(null);
         } else {
-            final List<String> files = Arrays.asList(rootCodesWorkbenchLandingStrip.list());
-            Collections.sort(files, String.CASE_INSENSITIVE_ORDER);
-            return files;
+            return collectFileNames(rootCodesWorkbenchLandingStrip::list);
         }
+    }
+
+    private List<String> collectFileNames(final Supplier<String[]> filesArraySupplier) {
+        return Stream.of(filesArraySupplier.get())
+            .sorted(String.CASE_INSENSITIVE_ORDER)
+            .collect(Collectors.toList());
     }
 
     @NotNull
     @Override
     public MaterialComponentsResponse getMaterialBySubNumber(
         @NotNull final String subNumber, @Nullable final String setNumber, @Nullable final String titleId) {
-        final List<MaterialComponent> filteredComponents = new ArrayList<>();
         String responseMessage;
+        List<MaterialComponent> components;
 
         try {
-            for (final MaterialComponent component : getMaterialComponents(subNumber, setNumber)) {
-                if (isValidComponent(component)) {
-                    filteredComponents.add(component);
-                }
-            }
-            responseMessage = filteredComponents.isEmpty() ? SAP_COMPONENTS_NOT_FOUND_MESSAGE : HttpStatus.OK.name();
+            components = getMaterialComponents(subNumber, setNumber)
+                .distinct()
+                .parallel()
+                .filter(this::isValidComponent)
+                //Here we're merge elements with the same material numbers (bomComponent)
+                .collect(Collectors.toMap(MaterialComponent::getBomComponent, Function.identity(), this::mergeSapComponents))
+                .values()
+                .stream()
+                .sorted(materialComponentComparatorProvider.getComparator(titleId))
+                .collect(Collectors.toList());
+
+            responseMessage = components.isEmpty() ? SAP_COMPONENTS_NOT_FOUND_MESSAGE : HttpStatus.OK.name();
         } catch (final HttpStatusCodeException e) {
             LOG.error(e.getMessage(), e);
             responseMessage = getMessageByError(e.getResponseBodyAsString());
+            components = Collections.emptyList();
         }
 
-        Collections.sort(filteredComponents, materialComponentComparatorProvider.getComparator(titleId));
-        return new MaterialComponentsResponse(String.format(responseMessage, subNumber), filteredComponents);
+        return new MaterialComponentsResponse(String.format(responseMessage, subNumber), components);
     }
 
-    private Set<MaterialComponent> getMaterialComponents(@NotNull final String subNumber, @Nullable final String setNumber) {
-        final Set<MaterialComponent> components = new HashSet<>(sapService.getMaterialByNumber(subNumber).getComponents());
-        if (StringUtils.isNotBlank(setNumber)) {
-            components.addAll(sapService.getMaterialByNumber(setNumber).getComponents());
-        }
-        return components;
+    private Stream<MaterialComponent> getMaterialComponents(@NotNull final String subNumber, @Nullable final String setNumber) {
+        final Stream<MaterialComponent> subNumberComponents = sapService.getMaterialByNumber(subNumber)
+            .getComponents()
+            .stream();
+
+        return Optional.ofNullable(setNumber)
+            .filter(StringUtils::isNotBlank)
+            .map(sapService::getMaterialByNumber)
+            .map(Material::getComponents)
+            .map(Collection::stream)
+            .map(setNumberComponents -> Stream.concat(subNumberComponents, setNumberComponents))
+            .orElse(subNumberComponents);
     }
 
     private boolean isValidComponent(final MaterialComponent component) {
@@ -193,6 +190,12 @@ public class EditBookDefinitionServiceImpl implements EditBookDefinitionService 
             && isNotFuture;
     }
 
+    private MaterialComponent mergeSapComponents(final MaterialComponent existentComponent, final MaterialComponent newComponent) {
+        final Instant existentEffDate = existentComponent.getEffectiveDate().toInstant();
+        final Instant newEffDate = newComponent.getEffectiveDate().toInstant();
+        return existentEffDate.isBefore(newEffDate) ? newComponent : existentComponent;
+    }
+
     private String getMessageByError(final String errorText) {
         final String message;
         if (errorText.toLowerCase().contains("not found")) {
@@ -200,7 +203,7 @@ public class EditBookDefinitionServiceImpl implements EditBookDefinitionService 
         } else if (errorText.toLowerCase().contains("enter a valid")) {
             message = INVALID_SUB_NUMBER_MESSAGE;
         } else {
-            message = errorText + StringUtils.SPACE + SUB_NUMBER_MESSAGE;
+            message = String.join(StringUtils.SPACE, errorText, SUB_NUMBER_MESSAGE);
         }
         return message;
     }
