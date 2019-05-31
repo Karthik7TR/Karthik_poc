c3705e969af5f1b8e3264b6078418ae88504a26b
uc228424 <uc228424@ten.tfs.local>
2019-05-31 13:22:04 +0000

    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C1082639

--- ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/cleanup/JobCleaner.java
+++ ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/cleanup/JobCleaner.java
@@ -19,14 +19,12 @@ public class JobCleaner {
     public JobCleaner(
             final ManagerService managerService,
             @Value("${cleanup.jobs.older.than.this.many.days.old}")final int cleanJobsGreaterThanThisManyDaysOld,
-            @Value("${number.last.major.version.kept}")final int numberLastMajorVersionKept,
-            @Value("${days.before.docmetadata.delete}")final int daysBeforeDocMetadataDelete,
             @Value("${cleanup.cwb.files.older.than.this.many.days.old}")final int cleanCwbFilesGreaterThanThisManyDaysOld) {
         this.managerService = managerService;
         this.cleanJobsGreaterThanThisManyDaysOld = cleanJobsGreaterThanThisManyDaysOld;
         cleanPlannedOutagesGreaterThanThisManyDaysOld = 180;
-        this.numberLastMajorVersionKept = numberLastMajorVersionKept;
-        this.daysBeforeDocMetadataDelete = daysBeforeDocMetadataDelete;
+        numberLastMajorVersionKept = 2;
+        daysBeforeDocMetadataDelete = 30;
         this.cleanCwbFilesGreaterThanThisManyDaysOld = cleanCwbFilesGreaterThanThisManyDaysOld;
     }
 
