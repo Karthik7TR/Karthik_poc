49aa6586efb1a93b23f8be4b0990582fd8a97cef
uc228424 <uc228424@ten.tfs.local>
2019-06-06 09:46:34 +0000

    config rollback
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C1083869

--- ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/cleanup/BookDefinitionLockCleaner.java
+++ ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/cleanup/BookDefinitionLockCleaner.java
@@ -3,22 +3,15 @@ package com.thomsonreuters.uscl.ereader.mgr.cleanup;
 import static com.thomsonreuters.uscl.ereader.core.book.domain.BookDefinitionLock.LOCK_TIMEOUT_SEC;
 
 import com.thomsonreuters.uscl.ereader.mgr.web.service.book.BookDefinitionLockService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
 
 /**
  * A regularly scheduled task to clean up the BOOK_DEFINITION_LOCK table.
  * Removes locks that has expired
  */
-@Component
 public class BookDefinitionLockCleaner {
-    private final BookDefinitionLockService lockService;
-
-    @Autowired
-    public BookDefinitionLockCleaner(final BookDefinitionLockService lockService) {
-        this.lockService = lockService;
-    }
+    private BookDefinitionLockService lockService;
 
     /**
      * Clean up the BOOK_DEFINITION_LOCK table every 2 minutes.
@@ -26,5 +19,10 @@ public class BookDefinitionLockCleaner {
     @Scheduled(fixedRate = LOCK_TIMEOUT_SEC * 1000)
     public void cleanBookDefinitionLockTable() {
         lockService.cleanExpiredLocks();
+    }
+
+    @Required
+    public void setBookDefinitionLockService(final BookDefinitionLockService service) {
+        lockService = service;
     }
 }
