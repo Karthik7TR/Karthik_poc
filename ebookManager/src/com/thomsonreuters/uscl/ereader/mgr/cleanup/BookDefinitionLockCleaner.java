429224464c6baf28b7a734d44cb755304a0cf0c9
uc228424 <uc228424@ten.tfs.local>
2019-06-06 09:33:35 +0000

    manager java config
    git-tfs-id: [https://tfsnpt.int.thomson.com:443/tfs/Cobalt_Collection/]$/Cobalt eBook Builder/Development;C1083866

--- ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/cleanup/BookDefinitionLockCleaner.java
+++ ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/cleanup/BookDefinitionLockCleaner.java
@@ -3,15 +3,22 @@ package com.thomsonreuters.uscl.ereader.mgr.cleanup;
 import static com.thomsonreuters.uscl.ereader.core.book.domain.BookDefinitionLock.LOCK_TIMEOUT_SEC;
 
 import com.thomsonreuters.uscl.ereader.mgr.web.service.book.BookDefinitionLockService;
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 /**
  * A regularly scheduled task to clean up the BOOK_DEFINITION_LOCK table.
  * Removes locks that has expired
  */
+@Component
 public class BookDefinitionLockCleaner {
-    private BookDefinitionLockService lockService;
+    private final BookDefinitionLockService lockService;
+
+    @Autowired
+    public BookDefinitionLockCleaner(final BookDefinitionLockService lockService) {
+        this.lockService = lockService;
+    }
 
     /**
      * Clean up the BOOK_DEFINITION_LOCK table every 2 minutes.
@@ -19,10 +26,5 @@ public class BookDefinitionLockCleaner {
     @Scheduled(fixedRate = LOCK_TIMEOUT_SEC * 1000)
     public void cleanBookDefinitionLockTable() {
         lockService.cleanExpiredLocks();
-    }
-
-    @Required
-    public void setBookDefinitionLockService(final BookDefinitionLockService service) {
-        lockService = service;
     }
 }
--- ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/security/CobaltUserDetailsContextMapper.java
+++ ebookManager/src/com/thomsonreuters/uscl/ereader/mgr/security/CobaltUserDetailsContextMapper.java
@@ -5,24 +5,28 @@ import java.util.Collection;
 import javax.naming.NamingException;
 import javax.naming.directory.Attributes;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.NotImplementedException;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ldap.core.AttributesMapper;
 import org.springframework.ldap.core.DirContextAdapter;
 import org.springframework.ldap.core.DirContextOperations;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.ldap.userdetails.UserDetailsContextMapper;
+import org.springframework.stereotype.Component;
 
 /**
  * Object mapper used to create a user object from the attributes contained within a specified LDAP directory context.
  * Delegates to a standard LDAP AttributesMapper to accomplish this.
  */
-
-@Slf4j
+@Component("userDetailsContextMapper")
 public class CobaltUserDetailsContextMapper implements UserDetailsContextMapper {
-    private AttributesMapper userEntryAttributesMapper;
+    private static final Logger log = LogManager.getLogger(CobaltUserDetailsContextMapper.class);
+    private final AttributesMapper userEntryAttributesMapper;
 
+    @Autowired
     public CobaltUserDetailsContextMapper(final AttributesMapper userEntryAttributesMapper) {
         this.userEntryAttributesMapper = userEntryAttributesMapper;
     }
