f1f3d79742adedd949e81fb95ec779e342bebd02
Kirill Uliashev <kirill.uliashev@thomsonreuters.com>
2020-01-28 15:03:23 +0300

    EB-2318: fix tests

--- ebookManager/srctest/com/thomsonreuters/uscl/ereader/mgr/web/controller/proviewlist/ProviewTitleListService_GetProviewTitlesTest.java
+++ ebookManager/srctest/com/thomsonreuters/uscl/ereader/mgr/web/controller/proviewlist/ProviewTitleListService_GetProviewTitlesTest.java
@@ -16,8 +16,6 @@ import java.util.List;
 
 import com.thomsonreuters.uscl.ereader.core.book.domain.BookDefinition;
 import com.thomsonreuters.uscl.ereader.core.book.domain.BookDefinition.PilotBookStatus;
-import com.thomsonreuters.uscl.ereader.core.book.model.Version;
-import com.thomsonreuters.uscl.ereader.core.book.util.BookTitlesUtil;
 import com.thomsonreuters.uscl.ereader.deliver.service.ProviewTitleInfo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -27,58 +25,61 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public final class ProviewTitleListService_GetProviewTitlesTest {
+    private static final String VERSION_1 = "v1.0";
+    private static final String VERSION_2 = "v1.1";
+    private static final String FINAL_STATUS = "Final";
+    private static final String REVIEW_STATUS = "Review";
+    private static final String REMOVED_STATUS = "Removed";
+    private static final String CLEANUP_STATUS = "Cleanup";
+
     @InjectMocks
     private ProviewTitleListServiceImpl service;
-    @Mock
-    private BookTitlesUtil bookTitlesUtil;
     @Mock
     private BookDefinition book;
 
     @Test
     public void shouldNotAllowRemovalAndPromotionIfBookNotFound() {
         // given
-        final List<ProviewTitleInfo> titleInfos = asList(titleInfo("v1.0", "Final"), titleInfo("v1.1", "Review"));
-        given(bookTitlesUtil.isSplitBook(any(BookDefinition.class), any(Version.class))).willReturn(false);
+        final List<ProviewTitleInfo> titleInfos = asList(titleInfo(VERSION_1, FINAL_STATUS), titleInfo(VERSION_2, REVIEW_STATUS));
         // when
         final List<ProviewTitle> titles = service.getProviewTitles(titleInfos, null);
         // then
         assertThat(titles, hasSize(2));
-        assertThat(titles, everyItem(isTitle(false, false)));
+        assertThat(titles, everyItem(isTitle(false, false, false)));
     }
 
     @Test
     public void shouldNotAllowPromotionInProgressBook() {
         // given
-        final List<ProviewTitleInfo> titleInfos = asList(titleInfo("v1.0", "Final"), titleInfo("v1.1", "Review"));
-        given(bookTitlesUtil.isSplitBook(eq(book), any(Version.class))).willReturn(false);
+        final List<ProviewTitleInfo> titleInfos = asList(titleInfo(VERSION_1, FINAL_STATUS), titleInfo(VERSION_2, REVIEW_STATUS));
         given(book.getPilotBookStatus()).willReturn(PilotBookStatus.IN_PROGRESS);
         // when
         final List<ProviewTitle> titles = service.getProviewTitles(titleInfos, book);
         // then
-        assertThat(titles.get(0), isTitle(true, false));
-        assertThat(titles.get(1), isTitle(true, false));
+        assertThat(titles.get(0), isTitle(true, false, false));
+        assertThat(titles.get(1), isTitle(true, false, false));
     }
 
-    @Test
-    public void shouldNotAllowRemovalAndPromotionOfSplitBook() {
-        // given
-        final List<ProviewTitleInfo> titleInfos = asList(titleInfo("v1.0", "Review"));
-        given(bookTitlesUtil.isSplitBook(eq(book), any(Version.class))).willReturn(true);
-        // when
-        final List<ProviewTitle> titles = service.getProviewTitles(titleInfos, book);
-        // then
-        assertThat(titles, contains(isTitle(false, false)));
-    }
 
     @Test
     public void shouldNotAllowPromotionOfFinalBook() {
         // given
-        final List<ProviewTitleInfo> titleInfos = asList(titleInfo("v1.0", "Final"), titleInfo("v1.1", "Review"));
-        given(bookTitlesUtil.isSplitBook(eq(book), any(Version.class))).willReturn(false);
+        final List<ProviewTitleInfo> titleInfos = asList(titleInfo(VERSION_1, FINAL_STATUS), titleInfo(VERSION_2, REVIEW_STATUS));
         // when
         final List<ProviewTitle> titles = service.getProviewTitles(titleInfos, book);
         // then
-        assertThat(titles.get(0), isTitle(true, false));
-        assertThat(titles.get(1), isTitle(true, true));
+        assertThat(titles.get(0), isTitle(true, false, false));
+        assertThat(titles.get(1), isTitle(true, true, false));
+    }
+
+    @Test
+    public void shouldAllowDeletionOfRemoveOrCleanupBook() {
+        // given
+        final List<ProviewTitleInfo> titleInfos = asList(titleInfo(VERSION_1, REMOVED_STATUS), titleInfo(VERSION_2, CLEANUP_STATUS));
+        // when
+        final List<ProviewTitle> titles = service.getProviewTitles(titleInfos, book);
+        // then
+        assertThat(titles.get(0), isTitle(false, false, true));
+        assertThat(titles.get(1), isTitle(false, false, true));
     }
 }
