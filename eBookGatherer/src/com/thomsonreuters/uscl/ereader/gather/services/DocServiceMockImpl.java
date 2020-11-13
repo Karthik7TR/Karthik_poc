1a00f0b4f72fa66594948c65e7a905e34ae6f0bd
NikolayVityazevTR <60702615+NikolayVityazevTR@users.noreply.github.com>
2020-11-13 19:25:13 +0300

    Eb 3155 updated mock inline index data for gaevidence book (#146)
    * EB-3155 - added inline index mock to gaevidence and gaevidence-small; updated mock gaevidence with pagebreaks <page>

--- eBookGatherer/src/com/thomsonreuters/uscl/ereader/gather/services/DocServiceMockImpl.java
+++ eBookGatherer/src/com/thomsonreuters/uscl/ereader/gather/services/DocServiceMockImpl.java
@@ -5,7 +5,7 @@ import com.thomsonreuters.uscl.ereader.gather.exception.GatherException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.SneakyThrows;
-import org.apache.commons.io.FileUtils;
+import com.thomsonreuters.uscl.ereader.core.book.util.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,12 +15,16 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static com.thomsonreuters.uscl.ereader.gather.services.DocServiceMockImpl.DocCollectionNames.GAEVIDENCE_INDEX;
+import static com.thomsonreuters.uscl.ereader.gather.services.DocServiceMockImpl.DocCollectionNames.GAEVIDENCE_SMALL_INDEX;
 import static java.util.Optional.ofNullable;
 
 @Component(value = "docServiceMockImpl")
 public class DocServiceMockImpl implements DocService {
+    private static final String XML = ".xml";
     @Autowired
     private ServletContext servletContext;
 
@@ -37,7 +41,8 @@ public class DocServiceMockImpl implements DocService {
         final boolean useReloadContent)
         throws GatherException {
         return ofNullable(DocCollectionNames.MAP.get(collectionName))
-                .map(docsLocation -> copyDocs(docGuids, docsLocation, contentDestinationDirectory))
+                .map(docsLocation -> copyDocs(docGuids, docsLocation, contentDestinationDirectory, collectionName))
+                .map(var -> constructResponse(docGuids))
                 .orElseGet(() -> docService.fetchDocuments(
                         docGuids,
                         collectionName,
@@ -49,15 +54,28 @@ public class DocServiceMockImpl implements DocService {
     }
 
     @SneakyThrows
-    private GatherResponse copyDocs(final Collection<String> docGuids, final String sourceDir, final File contentDestinationDirectory) {
-        FileUtils.copyDirectory(new File(servletContext.getRealPath(sourceDir)), contentDestinationDirectory);
+    private boolean copyDocs(final Collection<String> docGuids, final String sourceDir,
+                                    final File contentDestinationDirectory, final String collectionName) {
+        final File sourceDirectory = new File(servletContext.getRealPath(sourceDir));
+        if (Objects.equals(collectionName, GAEVIDENCE_INDEX.getDocCollectionName()) ||
+            Objects.equals(collectionName, GAEVIDENCE_SMALL_INDEX.getDocCollectionName())) {
+            docGuids.forEach(docGuid ->
+                FileUtils.copyFileToDirectory(new File(sourceDirectory, docGuid + XML), contentDestinationDirectory)
+            );
+        } else {
+            FileUtils.copyDirectory(sourceDirectory, contentDestinationDirectory);
+        }
 
+        return true;
+    }
+
+    @SneakyThrows
+    private GatherResponse constructResponse(final Collection<String> docGuids) {
         final GatherResponse gatherResponse = new GatherResponse();
         gatherResponse.setNodeCount(docGuids.size());
         gatherResponse.setDocCount(docGuids.size());
         gatherResponse.setDocCount2(docGuids.size());
         gatherResponse.setPublishStatus("DOC Gather Completed");
-
         return gatherResponse;
     }
 
@@ -67,7 +85,9 @@ public class DocServiceMockImpl implements DocService {
         MDLITMAN("mock_mdlitman", "/WEB-INF/poc/Novus_Novus_POC/annualPamphlets/mdlitman/Docs"),
         MDLITMAN_SMALL("mock_mdlitman_small", "/WEB-INF/poc/Novus_Novus_POC/annualPamphlets/mdlitman-small/Docs"),
         GAEVIDENCE("mock_gaevidence", "/WEB-INF/poc/Novus_Novus_POC/annualPamphlets/gaevidence/Docs"),
+        GAEVIDENCE_INDEX("mock_gaevidence_index", "/WEB-INF/poc/Novus_Novus_POC/annualPamphlets/gaevidence/Index"),
         GAEVIDENCE_SMALL("mock_gaevidence_small", "/WEB-INF/poc/Novus_Novus_POC/annualPamphlets/gaevidence-small/Docs"),
+        GAEVIDENCE_SMALL_INDEX("mock_gaevidence_small_index", "/WEB-INF/poc/Novus_Novus_POC/annualPamphlets/gaevidence-small/Index"),
         THORBURN("mock_thorburn", "/WEB-INF/poc/Novus_Novus_POC/eLooseleafs/thorburn/Docs"),
         THORBURN_SMALL("mock_thorburn_small", "/WEB-INF/poc/Novus_Novus_POC/eLooseleafs/thorburn-small/Docs"),
         FCF_SMALL("mock_fcf_small", "/WEB-INF/poc/Novus_Novus_POC/mockBooks/fletcherCorporationForms-small/Docs");
